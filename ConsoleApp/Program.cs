cea223a59eaae8f8b8a0f6d7f04a25417a6260b8
misikdmytro <misik.dmitriy@gmail.com>
2023-02-01 19:38:28 +0200

    float/double/decimal

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,3 +15,30 @@ Console.WriteLine(6 % 3);
 Console.WriteLine(7 % 3);
 Console.WriteLine(8 % 3);
 Console.WriteLine(9 % 3);
+
+float f = 1.1F;
+double d = 1.2;
+decimal dl = 1.3M;
+
+Console.WriteLine(f);
+Console.WriteLine(d);
+Console.WriteLine(dl);
+Console.WriteLine(d / 2.5);
+
+Console.WriteLine("FLOAT MATH");
+Console.WriteLine(6.0F / 3.0F);
+Console.WriteLine(7.0F / 3.0F);
+Console.WriteLine(8.0F / 3.0F);
+Console.WriteLine(9.0F / 3.0F);
+
+Console.WriteLine("DOUBLE MATH");
+Console.WriteLine(6.0 / 3.0);
+Console.WriteLine(7.0 / 3.0);
+Console.WriteLine(8.0 / 3.0);
+Console.WriteLine(9.0 / 3.0);
+
+Console.WriteLine("DECIMAL MATH");
+Console.WriteLine(6.0M / 3.0M);
+Console.WriteLine(7.0M / 3.0M);
+Console.WriteLine(8.0M / 3.0M);
+Console.WriteLine(9.0M / 3.0M);
