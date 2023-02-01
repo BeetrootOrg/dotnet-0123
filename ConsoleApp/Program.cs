f1bb4dce0e8c42ef0e41b5155d215c1fbd6c12b6
misikdmytro <misik.dmitriy@gmail.com>
2023-02-01 19:30:41 +0200

    operations in c#

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿int i = 42;
-Console.WriteLine(i);
+﻿int a = 42;
+int b = 41;
+int c = a + b;
+
+Console.WriteLine(a + b);
+Console.WriteLine(c);
+
+Console.WriteLine(6 / 3);
+Console.WriteLine(7 / 3);
+Console.WriteLine(8 / 3);
+Console.WriteLine(9 / 3);
+
+Console.WriteLine("RESULT OF %");
+Console.WriteLine(6 % 3);
+Console.WriteLine(7 % 3);
+Console.WriteLine(8 % 3);
+Console.WriteLine(9 % 3);
