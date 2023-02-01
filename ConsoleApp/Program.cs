af8c82a53d934f0ab7292d977875b2bd4927d1ca
Andrey Tzarkov <admyndc@gmail.com>
2023-02-01 23:12:16 +0200

    change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a=42;
+int b=41;
+int c=a+b;
+Console.WriteLine(a+b);
+Console.WriteLine(c);
+Console.WriteLine(@"RESULT OF /");
+Console.WriteLine(6/3);
+Console.WriteLine(7/3);
+Console.WriteLine(8/3);
+Console.WriteLine(9/3);
+Console.WriteLine("RESULT OF %");
+Console.WriteLine(6%3);
+Console.WriteLine(7%3);
+Console.WriteLine(8%3);
+Console.WriteLine(9%3);
+float f=1.1F;
+double d=1.2;
+decimal dl=1.3M;
+
