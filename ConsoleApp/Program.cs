671d5d32e51ebb3969e21453be97d08ca1bda9f9
misikdmytro <misik.dmitriy@gmail.com>
2023-02-01 19:49:29 +0200

    comparison operator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,9 @@ int c = a + b;
 Console.WriteLine(a + b);
 Console.WriteLine(c);
 
+a += b;
+Console.WriteLine(a);
+
 Console.WriteLine(6 / 3);
 Console.WriteLine(7 / 3);
 Console.WriteLine(8 / 3);
@@ -42,3 +45,32 @@ Console.WriteLine(6.0M / 3.0M);
 Console.WriteLine(7.0M / 3.0M);
 Console.WriteLine(8.0M / 3.0M);
 Console.WriteLine(9.0M / 3.0M);
+
+bool b1 = true;
+bool b2 = false;
+
+Console.WriteLine(b1);
+Console.WriteLine(b2);
+
+Console.WriteLine("AND");
+Console.WriteLine(b1 && b2);
+Console.WriteLine($"true and true = {true && true}");
+Console.WriteLine($"false and true = {false && true}");
+Console.WriteLine($"true and false = {true && false}");
+Console.WriteLine($"false and false = {false && false}");
+
+Console.WriteLine("OR");
+Console.WriteLine(b1 || b2);
+Console.WriteLine($"true and true = {true || true}");
+Console.WriteLine($"false and true = {false || true}");
+Console.WriteLine($"true and false = {true || false}");
+Console.WriteLine($"false and false = {false || false}");
+
+Console.WriteLine("COMPARISON");
+int i1 = 42;
+int i2 = 42;
+int i3 = 43;
+Console.WriteLine($"i1 == i2 = {i1 == i2}");
+Console.WriteLine($"i1 == i3 = {i1 == i3}");
+Console.WriteLine($"i1 != i2 = {i1 != i2}");
+Console.WriteLine($"i1 != i3 = {i1 != i3}");
