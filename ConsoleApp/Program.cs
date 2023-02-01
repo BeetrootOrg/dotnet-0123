1e5dd09e558693860e945da2d054eb1229ca3395
Andrey Tzarkov <admyndc@gmail.com>
2023-02-01 23:12:33 +0200

    more change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,19 +2,45 @@
 int a=42;
 int b=41;
 int c=a+b;
+
 Console.WriteLine(a+b);
 Console.WriteLine(c);
+
 Console.WriteLine(@"RESULT OF /");
 Console.WriteLine(6/3);
 Console.WriteLine(7/3);
 Console.WriteLine(8/3);
 Console.WriteLine(9/3);
+
 Console.WriteLine("RESULT OF %");
 Console.WriteLine(6%3);
 Console.WriteLine(7%3);
 Console.WriteLine(8%3);
 Console.WriteLine(9%3);
+
 float f=1.1F;
 double d=1.2;
 decimal dl=1.3M;
 
+Console.WriteLine(f);
+Console.WriteLine(d);
+Console.WriteLine(dl);
+Console.WriteLine(d/2.5);
+
+Console.WriteLine("FLOAT MATH");
+Console.WriteLine(6F/3F);
+Console.WriteLine(7F/3F);
+Console.WriteLine(8F/3F);
+Console.WriteLine(9F/3F);
+
+Console.WriteLine("DOUBLE MATH");
+Console.WriteLine(6.0/3.0);
+Console.WriteLine(7.0/3.0);
+Console.WriteLine(8.0/3.0);
+Console.WriteLine(9.0/3.0);
+
+Console.WriteLine("DECIMAL MATH");
+Console.WriteLine(6M/3M);
+Console.WriteLine(7M/3M);
+Console.WriteLine(8M/3M);
+Console.WriteLine(9M/3M);
