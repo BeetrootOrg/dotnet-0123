a9d3e90a615bb165cba831b9e9bd1c23eb0d335c
igorbelovash <belovash2002@gmail.com>
2023-02-02 10:47:02 +0200

    comparison

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,25 @@
-﻿int i = 42;
-Console.WriteLine(i);
+﻿int a = 42;
+int b = 41;
+int c = a + b;
+Console.WriteLine(a + b);
+Console.WriteLine(c);
+
+Console.WriteLine(6 / 3);
+Console.WriteLine(7 / 3);
+Console.WriteLine(8 / 3);
+Console.WriteLine(9 / 3);
+
+Console.WriteLine("Result of %");
+Console.WriteLine(6 % 3);
+Console.WriteLine(7 % 3);
+Console.WriteLine(8 % 3);
+Console.WriteLine(9 % 3);
+
+Console.WriteLine("COMPARISON");
+int i1 = 42;
+int i2 = 42;
+int i3 = 43;
+Console.WriteLine($"i1 == i2 = {i1 == i2}");
+Console.WriteLine($"i1 == i3 = {i1 == i3}");
+Console.WriteLine($"i1 != i2 = {i1 != i2}");
+Console.WriteLine($"i1 != i3 = {i1 != i3}");
