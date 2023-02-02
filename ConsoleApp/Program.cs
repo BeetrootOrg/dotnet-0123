c30ca9fed1fdf2ef62b1c997ae9d3a3ccc037305
Andrey Tzarkov <admyndc@gmail.com>
2023-02-02 03:11:26 +0200

    all

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,34 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
-byte by=0;
-short sh=1000;
-int i=20000;
-long an=(by+sh)*20000;
-bool flag=true;
-char sym='s';
-string s="text";
-decimal n1=7.0M;
-double db=10.15;
-float n2=6.0F;
+﻿const int x=10;
+const int y=12;
+int sum=0;
+int X=x;
+int Y=y;
 
-Console.WriteLine($"(by={by}+sh={sh})*i={i}={an}");
-Console.WriteLine($"flag and a ={flag==(an<0)}");
-Console.WriteLine($"{s}\n{sym}");
-Console.WriteLine("DECIMAL/FLOAT");
-Console.WriteLine((double)6.0M/(double)7.0F*db);
-Console.WriteLine((float)n1/n2);
+if (x==y) sum=x;
+else
+{
+    if (x>y) {X=y;Y=X;}
+    for(var i=X;i<=Y;i++){ sum=sum+i; }
+}
+Console.WriteLine($"the sum of all numbers between {x}..{y} is {sum}");
 
-int x=30;
-Console.WriteLine($"-6*x^3+5*x^2-10*x+15, where x {x}={Math.Pow(-6*x,3)+5*x*5*x-10*x+15}");
-double rad=Math.PI/4;
-Console.WriteLine($"x={rad} abs(x)*sin(x)={Math.Abs(rad)*Math.Sin(rad)}");
-float y=-14;
-Console.WriteLine($"x={y}.  2*pi*x={2*Math.PI*y}");
-Console.WriteLine($"x={x} y={y}.  max(x, y)={Math.Max(x,(int)y)}");
-
-Console.WriteLine("Extra");
-DateTime today=DateTime.Now;
-int newyear=(new DateTime(2023,12,31)).DayOfYear;
-int y1=today.DayOfYear;
-int x1=newyear-y1;
-Console.WriteLine($"days left to New Year {x1}");
-Console.WriteLine($"days passed from New Year {y1}");
+Console.WriteLine("Extra:");
+int x1; int y1;
+sum=0;
+try{
+    x1=Convert.ToInt16(Console.ReadLine());
+    y1=Convert.ToInt16(Console.ReadLine());
+    if (x==y) sum=x; else
+    {
+        for(var i=x1;i<=y1;i++){ sum=sum+i; }
+    }
+    Console.WriteLine($"the sum of all numbers between {x1}..{y1} is {sum}");
+} catch {Console.WriteLine("Invalid input"); return;}   
+// string inv="invalid";
+// string? x1;
+// string? y1;
+// string ou;
+// try{
+//     x1=Console.ReadLine();
+//     y1=Console.ReadLine();
+//     ou=$"{x1}{y1}";
+//     if (string.IsNullOrEmpty(x1) || string.IsNullOrEmpty(y1)) {Console.WriteLine("Invalid input"); return;}
+//     if (ou.ToLower().Contains(inv)) {Console.WriteLine("Invalid input"); return;}
+// } catch {Console.WriteLine("Invalid input"); return;}
