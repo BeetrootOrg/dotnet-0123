4e3f2059cbba65b36bead92e45c50d1cf5edc983
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-03 17:12:36 +0200

    math func

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,3 +11,13 @@ Console.WriteLine($"var1+var2={var1+var2}");
 Console.WriteLine($"var3*var4={var3*var4}");
 Console.WriteLine($"var1-var2={var1-var2}");
 
+System.Console.WriteLine("Math functions");
+int x = 2;
+int y = 3;
+System.Console.WriteLine($"x={x}");
+System.Console.WriteLine($"y={y}");
+System.Console.WriteLine($"-6*x^3+5*x^2-10*x+15={-6*x*x*x+5*x*x-10*x+15}");
+System.Console.WriteLine($"abs(x)*sin(x)={Math.Abs(x)*Math.Sin(x)}");
+System.Console.WriteLine($"2*pi*x={2*Math.PI*x}");
+System.Console.WriteLine($"max(x, y)={Math.Max(x,y)}");
+
