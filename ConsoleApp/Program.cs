ba7ad6e8d2655869026c85811cfb41b77d400801
misikdmytro <misik.dmitriy@gmail.com>
2023-02-03 19:32:40 +0200

    sum

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -48,3 +48,11 @@ for (int i = 0; i < 10; ++i)
 {
     Console.WriteLine(i);
 }
+
+var sum = 0;
+for (int i = 0; i <= 10; ++i)
+{
+    sum += i;
+}
+
+Console.WriteLine($"Sum from 0 to 10 is equal to {sum}");
