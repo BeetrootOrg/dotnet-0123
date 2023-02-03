ad3f7cd80930094ab149c8488a434a6613803072
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-03 19:15:54 +0200

    first commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,30 +1,4 @@
-﻿float var1 = 44.2F;
-float var2 = 50.64F;
-int var3 = 42;
-int var4 = 43;
-Console.WriteLine("addition, subtraction, multiplication between variables");
-Console.WriteLine($"var1={var1}");
-Console.WriteLine($"var2={var2}");
-Console.WriteLine($"var3={var3}");
-Console.WriteLine($"var4={var4}");
-Console.WriteLine($"var1+var2={var1+var2}");
-Console.WriteLine($"var3*var4={var3*var4}");
-Console.WriteLine($"var1-var2={var1-var2}");
-
-System.Console.WriteLine("Math functions");
-int x = 2;
-int y = 3;
-System.Console.WriteLine($"x={x}");
-System.Console.WriteLine($"y={y}");
-System.Console.WriteLine($"-6*x^3+5*x^2-10*x+15={-6*x*x*x+5*x*x-10*x+15}");
-System.Console.WriteLine($"abs(x)*sin(x)={Math.Abs(x)*Math.Sin(x)}");
-System.Console.WriteLine($"2*pi*x={2*Math.PI*x}");
-System.Console.WriteLine($"max(x, y)={Math.Max(x,y)}");
-
-System.Console.WriteLine("Datetime task");
-DateTime DayToday = DateTime.Today;
-DateTime NewYear2023 = new DateTime(2023, 1, 1, 0, 0, 0);
-DateTime NewYear2024 = new DateTime(2024, 1, 1, 0, 0, 0);
-System.Console.WriteLine($"{(NewYear2024-DayToday).Days} days left to New Year");
-System.Console.WriteLine($"{(DayToday-NewYear2023).Days} days passed from New Year");
-
+﻿string name = "Maksym";
+if (name=="Maksym"){
+    System.Console.WriteLine("Hi, Maksym");
+}
