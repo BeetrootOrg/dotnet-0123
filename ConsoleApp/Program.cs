bbb62f04f53ab425cebf0011d064b4a45535438e
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-03 17:05:12 +0200

    variables and operations

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float var1 = 44.2F;
+float var2 = 50.64F;
+int var3 = 42;
+int var4 = 43;
+Console.WriteLine("addition, subtraction, multiplication between variables");
+Console.WriteLine($"var1={var1}");
+Console.WriteLine($"var2={var2}");
+Console.WriteLine($"var3={var3}");
+Console.WriteLine($"var4={var4}");
+Console.WriteLine($"var1+var2={var1+var2}");
+Console.WriteLine($"var3*var4={var3*var4}");
+Console.WriteLine($"var1-var2={var1-var2}");
+
