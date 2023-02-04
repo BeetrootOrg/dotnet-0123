4ed7fd37b56f3a75d0a463e1c981e31265afd90f
viktor-cnc <viktor0989035826@gmail.com>
2023-02-04 16:18:07 +0200

    bool variables

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -18,7 +18,7 @@ Console.WriteLine (8 % 3);
 Console.WriteLine (9 % 3);
 
 
-*/
+
 
 float f = 1.5F;
 double d = 1.2;
@@ -26,6 +26,12 @@ decimal dl = 1.3M;
 
 Console.WriteLine (f); Console.WriteLine (d); Console.WriteLine (dl);
 Console.WriteLine(d / 2.5);
+
+Console.WriteLine ("MATH -- float");
+Console.WriteLine (6 / 3F);
+Console.WriteLine (7 / 3F);
+Console.WriteLine (8 / 3F);
+Console.WriteLine (9 / 3F);
 
 
 Console.WriteLine ("MATH -- double");
@@ -39,3 +45,19 @@ Console.WriteLine (6 / 3.0M);
 Console.WriteLine (7 / 3.0M);
 Console.WriteLine (8 / 3.0M);
 Console.WriteLine (9 / 3.0M);
+
+
+*/
+
+bool b1 = true;
+bool b2 = false;
+
+Console.WriteLine(b1);
+Console.WriteLine(b2);
+
+Console.WriteLine("'AND'");
+Console.WriteLine(b1 && b2);
+Console.WriteLine($"true and true = {true && true}");
+Console.WriteLine($"true and false = {true && false}");
+Console.WriteLine($"false and true = {false && true}");
+Console.WriteLine($"false and false = {false  && false}");
