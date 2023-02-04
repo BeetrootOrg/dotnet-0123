8eab041a8666b335437998fcdf1e4015f8a3d42e
excellent <flarpzz08@gmail.com>
2023-02-04 21:03:39 +0100

    initial commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("X=");
+if(!int.TryParse(Console.ReadLine(), out int x))
+{
+    Console.WriteLine("Invalid input");
+    Environment.Exit(0);
+}
+
+Console.Write("Y=");
+if(!int.TryParse(Console.ReadLine(), out int y))
+{
+    Console.WriteLine("Invalid input");
+    Environment.Exit(0);
+}
+
+int min = x < y ? x : y;
+int max = x > y ? x : y;
+
+int sum = 0;
+for (int i = min; i <= max; i++)
+{
+    sum += i;
+}
+Console.Write($"Sum={sum}");
