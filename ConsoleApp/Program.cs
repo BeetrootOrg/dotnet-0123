49957fda5f9b762f64fea647596047c669bc6729
Alexander Strekanov <strekanov.alexander@gmail.com>
2023-02-04 10:32:12 +0200

    homework-04 commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ const int X = 5;
+ const int Y = 2;
+
+int min = Math.Min(X, Y);
+int max = Math.Max(X, Y);
+int sum = (min == max) ? min : 0;
+string str = "Sum=";
+
+if (sum == 0)
+{
+    for (int i = min; i <= max; i++)
+    {
+        str += (i == max) ? $"{i}=" : $"{i}+";
+        sum += i;
+    }
+}
+
+Console.WriteLine($"{str}{sum}");
+Console.WriteLine();
+
+// Extra
+Console.WriteLine("Enter X:");
+string? stringX = Console.ReadLine();
+Console.WriteLine("Enter Y:");
+string? stringY = Console.ReadLine();
+
+bool isX = int.TryParse(stringX, out int intX);
+bool isY = int.TryParse(stringY, out int intY);
+
+if (!(isX && isY))
+{
+    Console.WriteLine("Invalid input");
+    return;
+}
+
+Console.WriteLine($"X = {intX}; Y = {intY}");
