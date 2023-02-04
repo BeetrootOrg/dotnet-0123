9e5c8500bc179a6b4cffc368a7054fe440408463
viktor-cnc <viktor0989035826@gmail.com>
2023-02-04 15:59:28 +0200

    змінні float, decimal, double

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,41 @@
-﻿int i = 42;
-Console.WriteLine (i);
+﻿/*
+int a = 42;
+int b = 41;
+int c = a + b;
+
+Console.WriteLine (a + b);
+Console.WriteLine (c);
+
+Console.WriteLine (6 / 3);
+Console.WriteLine (7 / 3);
+Console.WriteLine (8 / 3);
+Console.WriteLine (9 / 3);
+
+Console.WriteLine ("result of %");
+Console.WriteLine (6 % 3);
+Console.WriteLine (7 % 3);
+Console.WriteLine (8 % 3);
+Console.WriteLine (9 % 3);
+
+
+*/
+
+float f = 1.5F;
+double d = 1.2;
+decimal dl = 1.3M;
+
+Console.WriteLine (f); Console.WriteLine (d); Console.WriteLine (dl);
+Console.WriteLine(d / 2.5);
+
+
+Console.WriteLine ("MATH -- double");
+Console.WriteLine (6 / 3.0);
+Console.WriteLine (7 / 3.0);
+Console.WriteLine (8 / 3.0);
+Console.WriteLine (9 / 3.0);
+
+Console.WriteLine ("MATH -- decimal");
+Console.WriteLine (6 / 3.0M);
+Console.WriteLine (7 / 3.0M);
+Console.WriteLine (8 / 3.0M);
+Console.WriteLine (9 / 3.0M);
