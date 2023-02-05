6ded4cb892aeb2e35dc8c5d59ad6eaaf858ab66c
IrynaDonienko <114020848+IrynaDonienko@users.noreply.github.com>
2023-02-05 12:05:25 +0100

    new code

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x;
+int y;
+int sum = 0;
+
+Console.WriteLine("Enter integer value 1");
+string? input = Console.ReadLine();
+bool check = int.TryParse(input, out x);
+if (check == false)
+{
+    Console.WriteLine("Not valid input. Try the program one more time");
+}
+else
+{
+    Console.WriteLine("Enter integer value 2");
+    input = Console.ReadLine();
+    check = int.TryParse(input, out y);
+    if (check == false)
+    {
+        Console.WriteLine("Not valid input. Try the program one more time");
+    }
+    else
+    {
+        if (x > y)
+        {
+            for (int i = y; i <= x; i++)
+            {
+                sum += i;
+            }
+        }
+        else if (x < y)
+        {
+            for (int i = x; i <= y; i++)
+            {
+                sum += i;
+            }
+        }
+        else sum = x;
+        System.Console.WriteLine($"The sum of all numbers between input numbers is {sum}");
+    }
+}
