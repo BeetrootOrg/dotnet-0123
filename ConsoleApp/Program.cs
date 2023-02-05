4de953a5f2825cb2793a610383d17d80ad739084
viktor-cnc <viktor0989035826@gmail.com>
2023-02-05 20:11:42 +0200

    тернарний оператор

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,33 @@
-﻿string name = "viktor";
+﻿string name = "vviktor";
 if (name == "Viktor")
-{Console.WriteLine($"name = {name}");}
+{ Console.WriteLine($"name = {name}"); }
 
 else if (name == "viktor")
 {
     Console.WriteLine($"name = {name}");
 }
 
-
 else
 {
     Console.WriteLine($"name is other = {name}");
 }
 System.Console.WriteLine("next operation");
+
+switch (name)
+{
+    case "Viktor":
+        { Console.WriteLine($"name = {name}"); }
+        break;
+
+    case "viktor":
+        { Console.WriteLine("name = viktor"); }
+        break;
+
+    default:
+        Console.WriteLine($"name is other = {name}");
+        break;
+}
+
+int a = 11;
+int b = (a == 10 ? 15:20);
+Console.WriteLine(b);
