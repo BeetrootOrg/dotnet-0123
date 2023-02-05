95698003e57024d1c52502b2bfc562c120185867
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-05 23:16:47 +0100

    new change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,30 @@
-﻿int i=42;
-Console.WriteLine(i);
+﻿int a=42;
+int b=41;
+int c=a+b;
+Console.WriteLine(a+b);
+Console.WriteLine(c);
+
+Console.WriteLine(6/3);
+Console.WriteLine(7/3);
+Console.WriteLine(8/3);
+Console.WriteLine(9/3);
+Console.WriteLine();
+
+Console.WriteLine(6%3);
+Console.WriteLine(7%3);
+Console.WriteLine(8%3);
+Console.WriteLine(9%3);
+
+float f=1.1F;
+double d=1.2;
+decimal dl=1.3M;
+Console.WriteLine();
+Console.WriteLine(f);
+Console.WriteLine(d);
+Console.WriteLine(dl);
+Console.WriteLine(d/2.5);
+
+Console.WriteLine(6.0/3.0);
+Console.WriteLine(7.0/3.0);
+Console.WriteLine(8.0/3.0);
+Console.WriteLine(9.0/3.0);
