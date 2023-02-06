67bfc1f0b996bb4cf80b610640fcc7f028cb4a45
Artem <shumckiy2004@gmail.com>
2023-02-06 19:17:43 +0200

    fibonachi

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 10;
+
+
+int fib1 = 1;
+int fib2 = 0;
+int fib3 = 0;
+
+for (int i = 1; i<= n; ++i)
+{
+    fib3 = fib1;
+    System.Console.WriteLine(fib1);
+    fib1 = fib1 + fib2;
+    fib2 = fib3;
+
+}
