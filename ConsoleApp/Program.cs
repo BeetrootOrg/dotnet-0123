3bddc3e48e3ad9f8066796d737581130d7e43178
Andrey Tzarkov <admyndc@gmail.com>
2023-02-06 19:56:43 +0200

    перерва

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -32,3 +32,19 @@ int Sum(int a,int b)=>a+b;
 int a=10;int b=20;
 
 Console.WriteLine($"Summ {a}+{b}={Sum(a,b)}");
+// -----------Value типи-----------------
+//Method(int param) - param передається як значення, зовнішна змінна не буде змінюваться
+void Assign42(int param){param=42;}
+Assign42(a);
+Console.WriteLine($"перевірка значення метод(велью тип) а={a}");
+// -----------ref типи-----------------
+//Method(ref int param) - param передається як полислання, зовнішна змінна буде змінюваться
+void AssignRef42(ref int param){param=42;}
+AssignRef42(ref a);
+Console.WriteLine($"перевірка значення метод(реф тип) а={a}");
+// -----------out типи-----------------
+//Method(out int param) - param передається як полислання, зовнішна змінна буде ініціалізована в методі
+void AssignOut42(out int param){param=42;}
+int ou;
+AssignOut42(out ou);
+Console.WriteLine($"перевірка значення метод(аут тип) au={ou}");
