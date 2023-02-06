c2b42640c54c47d7009cf11e35f12ff759e2e18d
igorbelovash <belovash2002@gmail.com>
2023-02-06 16:38:29 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,8 +8,8 @@ if (success)
     bool success1 = int.TryParse(y, out int  num2);
     if (success1)
     {
-        int min = num1 < num2 ? num1 : num2;
-        int max = num1 > num2 ? num1 : num2;
+        int min = num1 < num2 ? num1 : num2; // if {5 < 2}  {2 = num1} else {2 = num2}  
+        int max = num1 > num2 ? num1 : num2;// if {5 > 2} { 5 = num1} else {5 = num2}
         int sum = 0;
         for(int i = min; i <= max; ++i )
         {
