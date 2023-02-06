9e26673f0449b5f3372849ef5290aa984cf6f555
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-06 10:52:35 +0200

    sum of all numbers

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter x:");
+bool successX = int.TryParse(Console.ReadLine(),out int x);
+Console.WriteLine("Enter y:");
+bool successY = int.TryParse(Console.ReadLine(),out int y);
+if (successX && successY)
+{
+    int sum = 0;
+    do
+    {
+        sum += x++;
+    } while (y>=x);
+    Console.WriteLine($"Sum = {sum}");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
