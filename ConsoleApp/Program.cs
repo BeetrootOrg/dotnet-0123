ba6294a24e4523fd6e617e0c6469c79ca2efea02
Andrey Tzarkov <admyndc@gmail.com>
2023-02-06 18:53:09 +0200

    Classwork 05 methods

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿//----------06/02/2023-----------------
+//--------Classwork 05 methods --------
+// header 
+//- what is methods;- how to use methods;- ref and out parameters;- optional parameters;- methods overloading
