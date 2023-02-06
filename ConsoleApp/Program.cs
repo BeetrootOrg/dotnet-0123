118b8ccb6d6a865be2dba6a7bbd8bbf8aa6b3d51
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-06 20:08:37 +0200

    fibb 2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 7;
+int a1 = 1;
+int a2 = 1;
+int a = 0;
+for (int i = 1; i<= N; i++ )
+{
+    a = a1 + a2;
+    a1 = a2;
+    a2 = a;
+}
+Console.WriteLine (a);
