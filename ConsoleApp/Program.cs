7cab7733ff1b4a3c61a3c1c29d5c6123db70b77b
igorbelovash <belovash2002@gmail.com>
2023-02-06 09:45:12 +0200

    Sum of values

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,33 +1,30 @@
-﻿String s1 = "Hello world!";
-Console.WriteLine(s1);
-double d = 12.2;
-double d1 = 12.2;
-Console.WriteLine(d);
-decimal dl = 2.0M;
-Console.WriteLine(dl);
-float f = 12.2F;
-Console.WriteLine(f);
-Console.WriteLine($"d == f = {d == f}");
-Console.WriteLine($"d != f = {d != f}");
-Console.WriteLine($"d ==d1 = {d ==d1}");
-
-
-int x = 2;
-Console.WriteLine($"-6*x^3+5*x^2-10*x+15 = {-6 * Math.Pow(x, 3) + 5 * (x * x) - (10 * x) + 15}");
-Console.WriteLine($"abs(x)*sin(x) = {Math.Abs(x) * Math.Sin(x)}");   
-Console.WriteLine($"2*pi*x = {2 * Math.PI * x}");
-double x1 = 19.9;
-double y1 = 7.8;
-Console.WriteLine($"max(x, y) = {Math.Max(x1, y1)}");
- 
- Console.WriteLine("Extra task");
- DateTime Today =  DateTime.Now;
- DateTime DateNewYear = new DateTime(2023, 31, 12);
- DateTime DateYear = new DateTime(2022, 31, 12);
- TimeSpan  DaysLeft =  DateNewYear - Today ;
- TimeSpan  DaysPassed = Today - DateYear;
- Console.WriteLine($"days left to New Year = {DaysLeft}");
- Console.WriteLine($"days passed from New Year = {DaysPassed}");
-
+﻿Console.WriteLine("Enter the value X :");
+string? x = Console.ReadLine();
+bool success = int.TryParse(x, out int  num1);
+if (success)
+{
+    Console.WriteLine("Enter the value Y :");
+    string? y = Console.ReadLine();
+    bool success1 = int.TryParse(y, out int  num2);
+    if (success1)
+    {
+        int min = num1 < num2 ? num1 : num2;
+        int max = num1 > num2 ? num1 : num2;
+        int sum = 0;
+        for(int i = min; i <= max; ++i )
+        {
+            sum += i;
+        }
+        Console.WriteLine($"Sum of values = {sum}");
+    }
+    else
+    {
+        Console.WriteLine($"Invalid input ");
+    }
+}
+else
+{
+    Console.WriteLine($"Invalid input ");
+}
 
 
