b88c5d06e0d659f01cb9abd315b5e5eb6d96b26d
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-06 17:04:15 +0100

    new change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -28,3 +28,32 @@ Console.WriteLine(6.0/3.0);
 Console.WriteLine(7.0/3.0);
 Console.WriteLine(8.0/3.0);
 Console.WriteLine(9.0/3.0);
+
+
+bool b1=true;
+bool b2=false;
+Console.WriteLine(b1);
+Console.WriteLine(b2);
+
+Console.WriteLine("AND");
+System.Console.WriteLine(b1&&b2);
+System.Console.WriteLine($"true and true={true&&true}");
+System.Console.WriteLine($"false and true={false&&true}");
+System.Console.WriteLine($"true and false={true&&false}");
+System.Console.WriteLine($"false and false={false&&false}");
+
+Console.WriteLine("Or");
+System.Console.WriteLine(b1||b2);
+System.Console.WriteLine($"true or true={true||true}");
+System.Console.WriteLine($"false or true={false||true}");
+System.Console.WriteLine($"true or false={true||false}");
+System.Console.WriteLine($"false or false={false||false}");
+
+System.Console.WriteLine("comparison");
+int i1=42;
+int i2=42;
+int i3=43;
+System.Console.WriteLine($"i1 == i2 ={i1 == i2}");
+System.Console.WriteLine($"i1 == i3 ={i1 == i3}");
+System.Console.WriteLine($"i1 != i2 ={i1 != i2}");
+System.Console.WriteLine($"i1 != i3 ={i1 != i3}");
