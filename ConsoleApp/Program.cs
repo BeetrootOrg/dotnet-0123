7c0b7d5bf599b06a9292e6dd5adbf5d23864c3d2
Andrey Tzarkov <admyndc@gmail.com>
2023-02-06 19:33:41 +0200

    Фібоначи Цикл

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,12 +3,28 @@
 // header 
 //- what is methods;- how to use methods;- ref and out parameters;- optional parameters;- methods overloading
 
-Console.WriteLine("Фібоначи");
-const int i=10;
 
+const int i=10;
+//-----------Фібоначи Рекурсія---------------
+Console.WriteLine("Фібоначи Рекурсія");
 Console.WriteLine($"Fib={Fib(i)}");
 
 int Fib(int n)
 {
   return n > 1 ? Fib(n - 1) + Fib(n - 2) : n;
+}
+//-----------Фібоначи Цикл---------------
+Console.WriteLine("Фібоначи Цикл");
+
+int count=i;
+int fib1=1;
+int fib2=0;
+int fib3;
+
+while (count>0 ){
+    fib3=fib1+fib2;
+    Console.WriteLine($"Cur fib={fib3}");
+    fib1=fib2;
+    fib2=fib3;
+    count--;
 }
