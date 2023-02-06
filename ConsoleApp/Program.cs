657ea7339efe4f3db127e2e3051adc29a320c478
Andrey Tzarkov <admyndc@gmail.com>
2023-02-06 19:23:53 +0200

    Фібоначчі

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,3 +2,13 @@
 //--------Classwork 05 methods --------
 // header 
 //- what is methods;- how to use methods;- ref and out parameters;- optional parameters;- methods overloading
+
+Console.WriteLine("Фібоначи");
+const int i=10;
+
+Console.WriteLine($"Fib={Fib(i)}");
+
+int Fib(int n)
+{
+  return n > 1 ? Fib(n - 1) + Fib(n - 2) : n;
+}
