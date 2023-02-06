7f7b5891070ad562e9b432f716c82e078d76d39f
misikdmytro <misik.dmitriy@gmail.com>
2023-02-06 19:45:17 +0200

    increment

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,3 +14,23 @@ int a = 15;
 int b = 20;
 Console.WriteLine(Sum(a, b));
 Console.WriteLine(SumOneLine(42, 80));
+
+int inc = 0;
+void IncrementWithClosure()
+{
+    ++inc;
+}
+
+static void IncrementWithoutClosure()
+{
+    int inc = 0;
+#pragma warning disable IDE0059
+    ++inc;
+#pragma warning restore all
+}
+
+IncrementWithClosure();
+IncrementWithClosure();
+IncrementWithoutClosure();
+
+Console.WriteLine(inc);
