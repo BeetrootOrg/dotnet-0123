6e357dda071ddee8b9a6b047da1572172c1e197e
viktor-cnc <viktor0989035826@gmail.com>
2023-02-08 00:13:37 +0200

    домашня робота 03

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,96 @@
-﻿// Класиний урок №03 умовні оператори
-// Парсинг, безпечний парсинг з bool
-// DateTime
-// TimeSpan
+﻿// homework/03-Conditions & Loops
+
+/*
+Create a program that will start with declaration of two constants (X and Y) and will count the sum of all numbers between these constants. 
+If they are equal then sum should be one of them
+
+Example:
+
+X=10
+Y=12
+Sum=10+11+12=33
+
+X=5
+Y=2
+Sum=2+3+4+5=14
+
+X=10
+Y=10
+Sum=10
+
+Extra:
+
+Read values of X and Y from the console. If output is invalid - write to console Invalid input and exit the program.
+
+*/
+int x;
+int y;
+int suma;
+string input;
+
+
+// тут вводимо Х
+for (; ; )
+{
+    Console.WriteLine("Enter X");
+    input = Console.ReadLine();
+    bool isOk = int.TryParse(input, out int x1);
+    if (isOk)
+    {
+        x = x1;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("...");
+        Console.WriteLine("try again");
+        Console.WriteLine("...");
+    }
+
+}
+// Console.WriteLine(x);
+
+
+// тут вводимо У
+for (; ; )
+{
+    Console.WriteLine("Enter У");
+    input = Console.ReadLine();
+    bool isOk = int.TryParse(input, out int y1);
+    if (isOk)
+    {
+        y = y1;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("...");
+        Console.WriteLine("try again");
+        Console.WriteLine("...");
+    }
+
+}
+// Console.WriteLine(y);
+
+
+if (x == y)
+{
+    suma = x;
+}
+else
+{
+int maximum = Math.Max (x, y);
+int minimum = Math.Min (x, y);
+
+for (int i = minimum, i = maximum, ++i)
+}
+
+
+
+
+
+
+
 
 
 /*
@@ -199,12 +288,15 @@ int number = Convert.ToInt32 (input1);
 System.Console.WriteLine($"you entered {number}");
 */
 
-
-DateTime data = new DateTime (2025, 1, 10, 21, 54, 30);
+/*
+DateTime data = new DateTime(2025, 1, 10, 21, 54, 30);
 Console.WriteLine(data);
 
 DateTime data1 = DateTime.Now;
 Console.WriteLine(data1);
 
-TimeSpan ts1 = new TimeSpan (0, 70, 20);
+TimeSpan ts1 = new TimeSpan(0, 70, 20);
 Console.WriteLine(data + ts1);
+
+System.Console.WriteLine(data < data1);
+*/
