92dd9bce43c508ca142fafb1d9778014f5caa68c
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-08 19:17:16 +0200

    simpify

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,24 +1,20 @@
 ﻿int a = 5;
 long Factorial(int n)
 {
-    if (n==0)
+    if (n == 0)
     {
         return 1;
     }
-    return n*Factorial(n-1);
+    return n * Factorial(n - 1);
 }
 Console.WriteLine(Factorial(a));
 
 void WriteNumbers(int n)
 {
-    if (n ==1 )
+    Console.WriteLine(n);
+    if (n != 1)
     {
-        Console.WriteLine(1);
-    }
-    else
-    {
-        Console.WriteLine(n);
-        WriteNumbers(n-1);
+        WriteNumbers(n - 1);
     }
 }
 WriteNumbers(a);
