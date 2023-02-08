5c315833e2593918d70dcaec49f8e3d638e7599d
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-08 13:03:45 +0200

    max, min, trysumifodd

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int a = 10;
+const int b = 5;
+
+
+int Max(int a, int b, int c = -2147483648, int d = -2147483648)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    if (d > max)
+    {
+        max = d;
+    }
+    return max;
+}
+
+int Min(int a, int b, int c = 2147483647, int d = 2147483647)
+{
+    int min = a;
+    if (b < min)
+    {
+        min = b;
+    }
+    if (c < min)
+    {
+        min = c;
+    }
+    if (d < min)
+    {
+        min = d;
+    }
+    return min;
+}
+
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine($"Max(a, b) = {Max(a, b)}");
+Console.WriteLine($"Min(a, b) = {Min(a, b)}");
+
+bool TrySumIfOdd(int a, int b, out int result)
+{
+    result = a + b;
+    return ((a + b) % 2) == 1;
+}
+Console.WriteLine($"TrySumIfOdd(a, b) = {TrySumIfOdd(a, b, out int result)}. Result = {result}");
