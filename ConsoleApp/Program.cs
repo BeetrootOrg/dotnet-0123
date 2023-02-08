a06200aace2cef770a8d3ba5187bd4b9a72e7952
viktor-cnc <viktor0989035826@gmail.com>
2023-02-08 19:21:40 +0200

    other

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,107 +1,35 @@
-﻿string name = "vviktor";
-if (name == "Viktor")
-{ Console.WriteLine($"name = {name}"); }
+﻿// 1. Calculate N! = 1 * 2 * 3 .. * N
+// Example:
+// 0! = 1
+// 1! = 1
+// 2! = 2
+// 3! = 6 (1 * 2 * 3)
+// 4! = 24 (1 * 2 * 3 * 4)
 
-else if (name == "viktor")
+// System.Console.WriteLine("Enter n:");
+// int n = Console.ReadLine ();
+
+
+int init = 1;
+int n = 5;
+long value = 1;
+
+
+/*
+long Factorial(int n,)
 {
-    Console.WriteLine($"name = {name}");
+    sum = ;
+    if (init != n )
+    
+    return 42;
 }
 
-else
+*/
+
+for (int i = init; i <= n; ++i)
 {
-    Console.WriteLine($"name is other = {name}");
-}
-System.Console.WriteLine("next operation");
-
-switch (name)
-{
-    case "Viktor":
-        { Console.WriteLine($"name = {name}"); }
-        break;
-
-    case "viktor":
-        { Console.WriteLine("name = viktor"); }
-        break;
-
-    default:
-        Console.WriteLine($"name is other = {name}");
-        break;
+    value *= i;
 }
 
-int a = 11;
-int b = (a == 10 ? 15 : 20);
-Console.WriteLine(b);
+System.Console.WriteLine(value);
 
-
-for (int i = 0; i < 10; ++i)
-{
-    Console.WriteLine(i);
-};
-
-
-var sum = 0;
-for (int i = 0; i <= 10; ++i)
-{
-    sum += i;
-}
-Console.WriteLine($"sum from 0 to 10 = {sum}");
-
-Console.WriteLine("MULTIPLICATION");
-
-var mul = 1;
-for (int i = 1; i <= 10; ++i)
-{
-    mul *= i;
-}
-Console.WriteLine(mul);
-
-mul = 1;
-int j = 1;
-while (j <= 10)
-{
-    mul *= j++;
-
-}
-Console.WriteLine(mul);
-
-
-// for (;;) -- можна опускати аргументи
-
-
-mul = 1;
-j = 1;
-do mul *= j++;
-while (j <= 10);
-
-Console.WriteLine(mul);
-
-while (false)
-{
-    Console.WriteLine("never here");
-}
-
-do {
-    Console.WriteLine("only once");
-    }
-while (false);
-
-sum = 0;
-for (int i = 0; i <= 42; i += 2)
-{
-sum += i;
-}
-Console.WriteLine($"sum of even from 0 to 42 = {sum}");
-
-sum = 0;
-for (int i = 1; i <= 42; i += 2)
-{
-sum += i;
-}
-Console.WriteLine($"sum of odd from 0 to 42 = {sum}");
-
-sum = 0;
-for (int i = 0; i <= 42; ++i)
-{
-sum += i;
-}
-Console.WriteLine($"sum from 0 to 42 = {sum}");
