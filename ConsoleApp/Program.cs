dfe7cbdd3dc05f6ad3d741e152430b584b04f087
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-08 16:15:18 +0100

    homework I.vyshnevskyi

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 1;
+int y = 2;
+System.Console.WriteLine($"-6*X^3+5*x^2-10*x+15 = {-6*Math.Pow(x,3) + 5*Math.Pow(x,2)-10*x+15 }");
+System.Console.WriteLine($"abs(x)*sin(x) = {Math.Abs(x)*Math.Sin(x)}");
+System.Console.WriteLine($"2*Pi*x = {2*Math.PI*x}");
+System.Console.WriteLine($"max(x,y) = {Math.Max(x,y)}");
