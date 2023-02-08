8653eb6615baddf6da8a62934e4a390f250821e8
Andrey Tzarkov <admyndc@gmail.com>
2023-02-08 19:23:38 +0200

    WriteNumbers

--- ConsoleApp/Classes/lesson06.cs
+++ ConsoleApp/Classes/lesson06.cs
@@ -13,6 +13,14 @@ namespace ConsoleApp.Classes
             if (n == 0) return 1;
             return n * Factorial(n - 1);
         }
+
+        public static void WriteNumbers(int n)
+        {
+            if (n>0){
+                Console.Write(n);
+                WriteNumbers(n-1);
+            }
+        }
     }
 }
 
