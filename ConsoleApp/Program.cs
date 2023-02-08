303b43b38c8607c59388edf4ff84b768ec8ea337
excellent <flarpzz08@gmail.com>
2023-02-08 18:12:38 +0100

    initial commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,56 +1,25 @@
-﻿// "Find the Maximum" functions
-int Max2(int a, int b)
+﻿// 1. Calculate N! = 1 * 2 * 3 .. * N
+// Example:
+// 0! = 1
+// 1! = 1
+// 2! = 2
+// 3! = 6 (1 * 2 * 3)
+// 4! = 24 (1 * 2 * 3 * 4)
+long Factorial(int n)
 {
-    return a > b ? a : b;
+    return n == 0
+    ? 1
+    : n * Factorial(n - 1);
 }
-int Max3(int a, int b, int c)
-{
-    return Max2(Max2(a, b), Max2(b, c));
-}
-int Max4(int a, int b, int c, int d)
-{
-    return Max2(Max3(a, b, c), d);
-}
+Console.WriteLine(Factorial(5));
 
-// "Find the Minimum" functions
-int Min2(int a, int b)
+// 2. Write all numbers from N to 1 in Console
+// 5 -> 5 4 3 2 1
+void WriteNumbers(int n)
 {
-    return a < b ? a : b;
+    for (int i = n; i >= 1; i--)
+    {
+        Console.Write($"{i} ");
+    }
 }
-int Min3(int a, int b, int c)
-{
-    return Min2(Min2(a, b), Min2(b, c));
-}
-int Min4(int a, int b, int c, int d)
-{
-    return Min2(Min3(a, b, c), d);
-}
-
-bool TrySumIfOdd(int a, int b, out int result)
-{
-    result = a + b;
-    return result % 2 != 0;
-}
-
-// Extra exercise
-string Repeat(string str, int number)
-{
-    return number <= 0
-    ? ""
-    : number > 1
-    ? str + Repeat(str, number - 1)
-    : str;
-}
-
-Console.WriteLine(Max2(2, 2));
-Console.WriteLine(Max3(3, 7, 8));
-Console.WriteLine(Max4(3, 7, 2, 5));
-
-Console.WriteLine(Min2(-1, 1));
-Console.WriteLine(Min3(3, 0, 9));
-Console.WriteLine(Min4(9, 7, 2, 4));
-
-Console.WriteLine(TrySumIfOdd(1, 6, out int result));
-Console.WriteLine(result);
-
-Console.WriteLine(Repeat("hell0", 4));
+WriteNumbers(5);
