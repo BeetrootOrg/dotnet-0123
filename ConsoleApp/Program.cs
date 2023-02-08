d3cf39bd3b16642d856a0a8cc35901dba69c9cd8
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-08 03:03:07 +0200

    variables and combinations

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,72 +1,28 @@
-﻿// int a = 42;
-// int b = 3;
-// int c = a + b;
+﻿//byte, short, int, long, bool, char, float, double, decimal, string
 
-// Console.WriteLine(a + b);
-// Console.WriteLine(c);
+byte b1 = 5;
+short s1 = 10;
+int i1 = 15;
+long l1 = 20;
 
-// Console.WriteLine(6/3);
-// Console.WriteLine(7/3);
-// Console.WriteLine(8/3);
-// Console.WriteLine(9/3);
+bool bo1 = true;
+char c1 = 'c';
 
-// Console.WriteLine(6%3);
-// Console.WriteLine(7%3);
-// Console.WriteLine(8%3);
-// Console.WriteLine(9%3);
+float f1 = 1.5F;
+double d1 = 1.6;
+decimal de1 = 3.14M;
 
-// float f = 1.1F;
-// double d = 1.2;
-// decimal d1 = 1.3M;
+string st1 = "help";
 
-// Console.WriteLine(f);
-// Console.WriteLine(d);
-// Console.WriteLine(d1);
-// Console.WriteLine(d / 2.5);
+System.Console.WriteLine($"byte = {b1}");
+System.Console.WriteLine($"short = {s1}");
+System.Console.WriteLine($"int = {i1}");
+System.Console.WriteLine($"long = {l1}");
+System.Console.WriteLine($"bool = {bo1}");
+System.Console.WriteLine($"char = {c1}");
+System.Console.WriteLine($"float = {f1}");
+System.Console.WriteLine($"double = {d1}");
+System.Console.WriteLine($"decimal = {de1}");
+System.Console.WriteLine($"string = {st1}");
 
-// Console.WriteLine( 6.0 / 3.0);
-// Console.WriteLine( 7.0 / 3.0);
-// Console.WriteLine( 8.0 / 3.0);
-// Console.WriteLine( 9.0 / 3.0);
-
-// Console.WriteLine( 6.0M / 3.0M);
-// Console.WriteLine( 7.0M / 3.0M);
-// Console.WriteLine( 8.0M / 3.0M);
-// Console.WriteLine( 9.0M / 3.0M);
-
-// Console.WriteLine( 6.0F / 3.0F);
-// Console.WriteLine( 7.0F / 3.0F);
-// Console.WriteLine( 8.0F / 3.0F);
-// Console.WriteLine( 9.0F / 3.0F);
-
-bool b1 = true;
-bool b2 = false;
-
-// System.Console.WriteLine(b1);
-// System.Console.WriteLine(b2);
-
-// System.Console.WriteLine( b1 && b2);
-// System.Console.WriteLine($"true && true = {true && true}");
-// System.Console.WriteLine($"true && false = {true && false}");
-// System.Console.WriteLine($"false && false = {false && false}");
-
-// System.Console.WriteLine( b1 || b2);
-// System.Console.WriteLine($"true || true = {true || true}");
-// System.Console.WriteLine($"true || false = {true || false}");
-// System.Console.WriteLine($"false || false = {false || false}");
-
-int i1 = 42;
-int i2 = 42;
-int i3 = 43;
-
-// System.Console.WriteLine($"i1 > i2 = {i1 > i2}");
-// System.Console.WriteLine($"i1 >= i2 = {i1 >= i2}");
-// System.Console.WriteLine($"i1 == i2 = {i1 == i2}");
-// System.Console.WriteLine($"i1 != i2 = {i1 != i2}");
-
-// System.Console.WriteLine($"i3 > i2 = {i3 > i2}");
-// System.Console.WriteLine($"i3 >= i2 = {i3 >= i2}");
-// System.Console.WriteLine($"i3 == i2 = {i3 == i2}");
-// System.Console.WriteLine($"i3 != i2 = {i3 != i2}");
-
-System.Console.WriteLine(6.0M / (decimal) 6.0F);
+System.Console.WriteLine($"b1 + s1 + i1 + l1 = {b1 + s1 + i1 + l1}"); //implicit casting to the same type
