8c9adbd0bbbf460c388c0d87352872400b68a6b7
Andrey Tzarkov <admyndc@gmail.com>
2023-02-08 19:31:50 +0200

    Debugging

--- ConsoleApp/Classes/lesson06.cs
+++ ConsoleApp/Classes/lesson06.cs
@@ -3,6 +3,11 @@ namespace ConsoleApp.Classes
 {
     public static class Lesson06
     {
+        public static int Sum(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
         public static void Test()
         {
             Console.WriteLine("Test");
@@ -16,9 +21,10 @@ namespace ConsoleApp.Classes
 
         public static void WriteNumbers(int n)
         {
-            if (n>0){
+            if (n > 0)
+            {
                 Console.Write(n);
-                WriteNumbers(n-1);
+                WriteNumbers(n - 1);
             }
         }
     }
