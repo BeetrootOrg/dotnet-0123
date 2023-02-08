b43c38976e200f282b9405b07683b29c163f645c
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-08 20:15:59 +0200

    console read

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,26 +1,17 @@
-﻿int Sum(int a, int b)
+﻿Random random = new((int)DateTime.Now.Ticks);
+// for debugging console read in launch.json write "console": "externalTerminal"
+System.Console.WriteLine("Press Enter to start");
+while (true)
 {
-    int sum = a+b;
-    return sum;
+    Console.ReadLine();
+    int num = random.Next(0,6);
+    if (num == 3)
+    {
+        System.Console.WriteLine("You`ve killed");
+        return;
+    }
+    else
+    {
+        System.Console.WriteLine("Continue");
+    }
 }
-
-static int Mul(int a, int b)
-{
-    int mul = a*b;
-    return mul;
-}
-int result = Sum(42,80);
-System.Console.WriteLine(result);
-
-result = Mul(10,12);
-System.Console.WriteLine(result);
-
-for (int i = 0; i < 5; i++)
-{
-    System.Console.WriteLine(i);
-}
-
-
-Random random = new((int)DateTime.Now.Ticks);
-System.Console.WriteLine(random.Next(10));
-
