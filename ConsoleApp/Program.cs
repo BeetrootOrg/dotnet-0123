6b718167d879a41ca82c6fb1054b4fe514ba655c
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-08 02:16:32 +0200

    Dividing

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,5 +2,29 @@
 int b = 3;
 int c = a + b;
 
-Console.WriteLine(a + b);
-Console.WriteLine(c);
+// Console.WriteLine(a + b);
+// Console.WriteLine(c);
+
+// Console.WriteLine(6/3);
+// Console.WriteLine(7/3);
+// Console.WriteLine(8/3);
+// Console.WriteLine(9/3);
+
+// Console.WriteLine(6%3);
+// Console.WriteLine(7%3);
+// Console.WriteLine(8%3);
+// Console.WriteLine(9%3);
+
+float f = 1.1F;
+double d = 1.2;
+decimal d1 = 1.3M;
+
+// Console.WriteLine(f);
+// Console.WriteLine(d);
+// Console.WriteLine(d1);
+// Console.WriteLine(d / 2.5);
+
+Console.WriteLine( 6.0 / 3.0);
+Console.WriteLine( 7.0 / 3.0);
+Console.WriteLine( 8.0 / 3.0);
+Console.WriteLine( 9.0 / 3.0);
