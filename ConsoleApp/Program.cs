01a265d3726a871468a5111a26c9479c4b5bab05
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-08 19:09:52 +0200

    done

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+long Factorial(int n)
+{
+    if (n==0)
+    {
+        return 1;
+    }
+    return n*Factorial(n-1);
+}
+Console.WriteLine(Factorial(a));
+
+void WriteNumbers(int n)
+{
+    if (n ==1 )
+    {
+        Console.WriteLine(1);
+    }
+    else
+    {
+        Console.WriteLine(n);
+        WriteNumbers(n-1);
+    }
+}
+WriteNumbers(a);
