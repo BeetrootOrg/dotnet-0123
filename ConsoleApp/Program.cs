4d4c10f7f1ac756dc96129d6cc323bb8511b0e9f
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-08 02:33:04 +0200

    boolean

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
-﻿int a = 42;
-int b = 3;
-int c = a + b;
+﻿// int a = 42;
+// int b = 3;
+// int c = a + b;
 
 // Console.WriteLine(a + b);
 // Console.WriteLine(c);
@@ -15,16 +15,42 @@ int c = a + b;
 // Console.WriteLine(8%3);
 // Console.WriteLine(9%3);
 
-float f = 1.1F;
-double d = 1.2;
-decimal d1 = 1.3M;
+// float f = 1.1F;
+// double d = 1.2;
+// decimal d1 = 1.3M;
 
 // Console.WriteLine(f);
 // Console.WriteLine(d);
 // Console.WriteLine(d1);
 // Console.WriteLine(d / 2.5);
 
-Console.WriteLine( 6.0 / 3.0);
-Console.WriteLine( 7.0 / 3.0);
-Console.WriteLine( 8.0 / 3.0);
-Console.WriteLine( 9.0 / 3.0);
+// Console.WriteLine( 6.0 / 3.0);
+// Console.WriteLine( 7.0 / 3.0);
+// Console.WriteLine( 8.0 / 3.0);
+// Console.WriteLine( 9.0 / 3.0);
+
+// Console.WriteLine( 6.0M / 3.0M);
+// Console.WriteLine( 7.0M / 3.0M);
+// Console.WriteLine( 8.0M / 3.0M);
+// Console.WriteLine( 9.0M / 3.0M);
+
+// Console.WriteLine( 6.0F / 3.0F);
+// Console.WriteLine( 7.0F / 3.0F);
+// Console.WriteLine( 8.0F / 3.0F);
+// Console.WriteLine( 9.0F / 3.0F);
+
+bool b1 = true;
+bool b2 = false;
+
+System.Console.WriteLine(b1);
+System.Console.WriteLine(b2);
+
+// System.Console.WriteLine( b1 && b2);
+// System.Console.WriteLine($"true && true = {true && true}");
+// System.Console.WriteLine($"true && false = {true && false}");
+// System.Console.WriteLine($"false && false = {false && false}");
+
+System.Console.WriteLine( b1 || b2);
+System.Console.WriteLine($"true || true = {true || true}");
+System.Console.WriteLine($"true || false = {true || false}");
+System.Console.WriteLine($"false || false = {false || false}");
