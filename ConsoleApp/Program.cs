5b6ad1faeacfd60eef74f9696d3200e0c4a225de
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-09 03:03:22 +0100

    git commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,3 +11,18 @@
     System.Console.WriteLine("Hi,not Illia");
  }
  System.Console.WriteLine("next operation");
+ 
+ System.Console.WriteLine("switch/case");
+ switch(name)
+ {
+    case "Illia":
+        System.Console.WriteLine("Hi,Illia");
+        break;
+    case "illia":
+        System.Console.WriteLine("Hi,illia");
+         break;
+    default:
+        System.Console.WriteLine("Hi,not Illia");
+        break;
+
+ }
