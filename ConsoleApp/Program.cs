a6ec95c64433afd53e43c8c263d1956774c22012
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-09 02:57:53 +0100

    classwork

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "illia";
+ if (name == "Illia"){
+    System.Console.WriteLine(("Hi,Illia"));
+ }
+ else if (name == "illia"){
+    System.Console.WriteLine("Hi,Illia");
+ }
+ else 
+ 
+ {
+    System.Console.WriteLine("Hi,not Illia");
+ }
+ System.Console.WriteLine("next operation");
