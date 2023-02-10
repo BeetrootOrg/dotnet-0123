09f8a235cc18468aa059a52720d1c3736f32186d
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-10 02:04:30 +0100

    new change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -26,3 +26,16 @@
         break;
 
  }
+ int a = 10;
+ int b = (a == 10 ? 15 : 20);//if (a == 10){ b = 15} else (b = 20)
+ System.Console.WriteLine(b);
+ //posible but not preferable
+ if (a == 10)
+     b = 15;
+ else 
+    b = 20;
+
+System.Console.WriteLine(("numbers from 0 to 9"));
+    for (int i = 0; i < 10; ++i){
+    System.Console.WriteLine(i);
+    }
