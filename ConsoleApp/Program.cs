3255bc60f2ed393e2d4a575375b8a3ed3df2ce16
Alexander Strekanov <strekanov.alexander@gmail.com>
2023-02-11 16:09:06 +0200

    homework-07 commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int[] CopyArray(int[] arr)
+{
+    int[] copy = new int[arr.Length];
+    Array.Copy(arr, copy, arr.Length);
+    return copy;
+}
+
+static int[] SelectionSort(int[] arr)
+{
+    int[] arrCopy = CopyArray(arr);
+    
+    for (int i = arrCopy.Length - 1; i >= 0; i--)
+    {
+        int indexOfMaxValue = i;
+        
+        for (int j = 0; j <= i; j++ )
+        {
+            if (arrCopy[j] > arrCopy[indexOfMaxValue])
+            {
+                indexOfMaxValue = j;
+            }
+        }
+        
+        if (indexOfMaxValue != i)
+        {
+            int value = arrCopy[i];
+            arrCopy[i] = arrCopy[indexOfMaxValue];
+            arrCopy[indexOfMaxValue] = value;
+        }
+    }
+    
+    return arrCopy;
+}
