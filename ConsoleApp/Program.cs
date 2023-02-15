e41931937bab22609e2d36760284b7ca83336e41
misikdmytro <misik.dmitriy@gmail.com>
2023-02-15 20:39:29 +0200

    sum

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,37 @@
-﻿DateTime nextNewYear = new(2024, 1, 1);
-DateTime now = DateTime.Now;
-// NOT THE SAME AS ABOVE
-// DateTime d1 = new DateTime();
+﻿string buffer;
 
-Console.WriteLine($"{now.DayOfYear} days passed from New Year");
-Console.WriteLine($"{(int)(nextNewYear - now).TotalDays} days left to New Year");
+Console.WriteLine("Enter X");
+buffer = Console.ReadLine();
+if (!int.TryParse(buffer, out int x))
+{
+    Console.WriteLine("Invalid input");
+    return;
+}
+
+Console.WriteLine("Enter Y");
+buffer = Console.ReadLine();
+if (!int.TryParse(buffer, out int y))
+{
+    Console.WriteLine("Invalid input");
+    return;
+}
+
+int min, max;
+if (x < y)
+{
+    min = x;
+    max = y;
+}
+else
+{
+    min = y;
+    max = x;
+}
+
+int sum = 0;
+for (int i = min; i <= max; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine(sum);
