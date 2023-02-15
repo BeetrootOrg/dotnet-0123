7f704aa10de567401c08013a85a30794ca255fc4
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-16 01:48:59 +0200

    recursion

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿int N = 10;
 int f_n1 = 1;
 int f_n2 = 1;
-for (int i = 3; i <= N; i++ )
+for (int i = 3; i <= N; i++)
 {
     int sum = f_n1 + f_n2;
     f_n1 = f_n2;
@@ -12,11 +12,11 @@ System.Console.WriteLine($"{N}-member of Fibonacci = {f_n2}");
 
 static int Sum(int a, int b)
 {
-   int sum = a + b;
-   return sum;  
+    int sum = a + b;
+    return sum;
 }
 
-static int SumOneLine (int a, int b) => a + b;
+static int SumOneLine(int a, int b) => a + b;
 
 int a = 10;
 int b = 5;
@@ -24,7 +24,7 @@ System.Console.WriteLine(Sum(a, b));
 System.Console.WriteLine(SumOneLine(a, b));
 
 int inc = 0;
-void Increment ()
+void Increment()
 {
     ++inc;
 }
@@ -34,7 +34,7 @@ Increment();
 
 System.Console.WriteLine(inc);
 
-void Assign42 (int param)
+void Assign42(int param)
 {
     param = 42;
 }
@@ -42,14 +42,14 @@ void Assign42 (int param)
 Assign42(a);
 System.Console.WriteLine(a);
 
-void Assign42Ref (ref int param)
+void Assign42Ref(ref int param)
 {
     param = 42;
 }
 Assign42Ref(ref a);
 System.Console.WriteLine(a);
 
-void Assign42Out (out int param)
+void Assign42Out(out int param)
 {
     param = 42;
 }
@@ -61,35 +61,35 @@ System.Console.WriteLine($"temp1 = {temp1}");
 Assign42Out(out int temp2);
 System.Console.WriteLine($"temp2 = {temp2}");
 
-void RefNoAssign(ref int param) {}
+void RefNoAssign(ref int param) { }
 // void OutNoAssign(out int param) {}
 
 int temp3 = 15;
 RefNoAssign(ref temp3);
 System.Console.WriteLine(temp3);
 
-bool TryReadInt(out int result)
-{
-    var input = Console.ReadLine();
-    return int.TryParse(input, out result);
+// bool TryReadInt(out int result)
+// {
+//     var input = Console.ReadLine();
+//     return int.TryParse(input, out result);
 
-    // if (int.TryParse(input, out result)) very ugly style
-    // {
-    //     return true;
-    // }
-    // else
-    // {
-    //     return false;
-    // }
-}
+//     // if (int.TryParse(input, out result)) very ugly style
+//     // {
+//     //     return true;
+//     // }
+//     // else
+//     // {
+//     //     return false;
+//     // }
+// }
 
-if (TryReadInt(out int temp4))
-{
-    System.Console.WriteLine(temp4);
-}
+// if (TryReadInt(out int temp4))
+// {
+//     System.Console.WriteLine(temp4);
+// }
 
 
-bool TryDivideByThree (int num, out int result)
+bool TryDivideByThree(int num, out int result)
 {
     if (num % 3 == 0)
     {
@@ -102,13 +102,13 @@ bool TryDivideByThree (int num, out int result)
         return false;
     }
 }
-var result1 =  TryDivideByThree (6, out temp4);
-System.Console.WriteLine($"TryDivideByThree(6) = {result1}. Result = {temp4}");
+// var result1 =  TryDivideByThree (6, out temp4);
+// System.Console.WriteLine($"TryDivideByThree(6) = {result1}. Result = {temp4}");
 
-result1 =  TryDivideByThree (7, out temp4);
-System.Console.WriteLine($"TryDivideByThree(7) = {result1}. Result = {temp4}");
+// result1 =  TryDivideByThree (7, out temp4);
+// System.Console.WriteLine($"TryDivideByThree(7) = {result1}. Result = {temp4}");
 
-bool TryDivideByThreeBetter (int num, out int result)
+bool TryDivideByThreeBetter(int num, out int result)
 {
     if (num % 3 == 0)
     {
@@ -119,19 +119,19 @@ bool TryDivideByThreeBetter (int num, out int result)
     return false;
 }
 
-bool TryDivideByThreeBest (int num, out int result)
+bool TryDivideByThreeBest(int num, out int result)
 {
     result = num / 3;
     return num % 3 == 0;
 }
 
-result1 =  TryDivideByThreeBetter  (7, out temp4);
-System.Console.WriteLine($"TryDivideByThreeBetter (7) = {result1}. Result = {temp4}");
+// result1 =  TryDivideByThreeBetter  (7, out temp4);
+// System.Console.WriteLine($"TryDivideByThreeBetter (7) = {result1}. Result = {temp4}");
 
-result1 =  TryDivideByThreeBest  (7, out temp4);
-System.Console.WriteLine($"TryDivideByThreeBest (7) = {result1}. Result = {temp4}");
+// result1 =  TryDivideByThreeBest  (7, out temp4);
+// System.Console.WriteLine($"TryDivideByThreeBest (7) = {result1}. Result = {temp4}");
 
-int Fibonacci (int n)
+int Fibonacci(int n)
 {
     // if (n == 1)
     // {
@@ -142,16 +142,50 @@ int Fibonacci (int n)
     //     return 1;
     // }
     // return Fibonacci(n - 1) + Fibonacci(n - 2);
-    return n == 1 
-        ? 1 
-        : n == 2 
-        ? 1 
-        : Fibonacci(n - 1) + Fibonacci (n - 2);
+    return n == 1
+        ? 1
+        : n == 2
+        ? 1
+        : Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-int temp5 = Fibonacci (3);
+int temp5 = Fibonacci(3);
 System.Console.WriteLine($"Fibonacci(5) = {temp5}");
 System.Console.WriteLine($"Fibonacci({N}) = {Fibonacci(N)}");
+
+void PrintHello(string name = "Elsa") // Optimal parameter
+{
+    System.Console.WriteLine($"Hello, {name}");
+}
+
+PrintHello("Mariana");
+PrintHello();
+
+void PrintNumbersLoop(int a, int b)
+{
+    for (int i = a; i <= b; i++)
+    {
+        System.Console.WriteLine(i);
+    }
+}
+
+void PrintNumbersRecursion(int a, int b)
+{
+    System.Console.WriteLine(a);
+    if (a != b)
+    {
+        PrintNumbersRecursion(a + 1, b);
+    }
+
+}
+
+PrintNumbersLoop(5, 15);
+PrintNumbersRecursion(5, 15);
+
+// bool TrySumIfOdd (int a, int b, out int result)
+// {
+
+// }
 
 
 
