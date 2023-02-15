501cffd1304210c21b350884b83efb8a7df82272
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-15 19:15:08 +0200

    chars

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿char c1 = 'c';
+char c2 = (char)99;
+char c3 = '\u0063'; //UTF = 16
+
+System.Console.WriteLine(c1);
+System.Console.WriteLine(c2);
+System.Console.WriteLine(c3);
+
+char c4 = '\'';
+System.Console.WriteLine(c4);
