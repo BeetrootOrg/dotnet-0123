6f59b0d888ab9419fd6ab14c24df2ba98310b04d
misikdmytro <misik.dmitriy@gmail.com>
2023-02-15 19:22:21 +0200

    char analyze

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,3 +8,31 @@ Console.WriteLine(c3);
 
 char c4 = '\'';
 Console.WriteLine(c4);
+
+static void DescribeChar(char c)
+{
+    Console.WriteLine($"Is ASCII = {char.IsAscii(c)}");
+    Console.WriteLine($"Is Control = {char.IsControl(c)}");
+    Console.WriteLine($"Is Digit = {char.IsDigit(c)}");
+    Console.WriteLine($"Is Letter = {char.IsLetter(c)}");
+    Console.WriteLine($"Is Lower = {char.IsLower(c)}");
+    Console.WriteLine($"Is Upper = {char.IsUpper(c)}");
+    Console.WriteLine($"Is Number = {char.IsNumber(c)}");
+    Console.WriteLine($"Is Punctuation = {char.IsPunctuation(c)}");
+    Console.WriteLine($"Is WhiteSpace = {char.IsWhiteSpace(c)}");
+}
+
+Console.WriteLine("Symbol 'c'");
+DescribeChar('c');
+
+Console.WriteLine("Symbol 'C'");
+DescribeChar('C');
+
+Console.WriteLine("Symbol ' '");
+DescribeChar(' ');
+
+Console.WriteLine("Symbol '4'");
+DescribeChar('4');
+
+Console.WriteLine("Symbol '\u1234'");
+DescribeChar('\u1234');
