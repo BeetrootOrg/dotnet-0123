b7d68a175a3e4be35e6391741340d21a20c87ed1
IrynaDonienko <114020848+IrynaDonienko@users.noreply.github.com>
2023-02-16 22:04:19 +0100

    array

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] SelectionSort(int[] array)
+{
+    int[] selected = new int[array.Length];
+    selected = array;
+    for (int i = 0; i < selected.Length; i++)
+    {
+        int smallest = i;
+        for (int j = i + 1; j < selected.Length; j++)
+        {
+            if (selected[j] < selected[smallest]) smallest = j;
+        }
+        int[] item = new int[1];
+        item[0] = selected[i];
+        selected[i] = selected[smallest];
+        selected[smallest] = item[0];
+    }
+    return selected;
+}
+
+int[] toSort = new int[] { 8, 4, -2, 100, 50, -40, 200, -300, 923, 90, 2, 0 };
+foreach (int r in SelectionSort(toSort))
+{
+    Console.WriteLine(r);
+}
