e847087e2931fdbe7ec80cca95a76f3b062e3512
Dmytro Misik <d.misik@draftkings.com>
2023-02-17 12:59:28 +0200

    sort

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void WriteLineArray(int[] array)
+{
+    Console.WriteLine(string.Join(",", array));
+}
+
+int[] Sort(int[] arr)
+{
+    return arr;
+}
+
+WriteLineArray(Sort(new[] { -10, -7, 1, 5, 6, 10 }));
+WriteLineArray(Sort(new[] { 10, 6, 5, 1, -7, -10 }));
+WriteLineArray(Sort(new[] { 5, 6, -10, 10, 1, -7 }));
+WriteLineArray(Sort(Array.Empty<int>()));
