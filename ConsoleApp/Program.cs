bf7ee67f9ed4e4ba1c393c9f818a92eaee3ee098
misikdmytro <misik.dmitriy@gmail.com>
2023-02-17 19:12:44 +0200

    booker

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,37 +1,11 @@
-﻿(int, string) tuple = (42, "hello");
-Console.WriteLine(tuple);
-
-(string, string) fullname1 = ("Dmytro", "Misik");
-Console.WriteLine(fullname1);
-Console.WriteLine(fullname1.Item1);
-Console.WriteLine(fullname1.Item2);
-
-(string, string) fullname2 = ("Dmytro", "Misik");
-Console.WriteLine(fullname1 == fullname2);
-
-static (int, string) GetSome()
+﻿static void Menu()
 {
-    return (42, "something");
+    Console.Clear();
+
+    Console.WriteLine("Meeting Booker");
+    Console.WriteLine();
+    Console.WriteLine("1. Create a meeting");
+    Console.WriteLine("0. Exit");
 }
 
-(int, string) result = GetSome();
-(int num, string some) = GetSome();
-(int a, string b) = result;
-
-Console.WriteLine(result);
-Console.WriteLine(num);
-Console.WriteLine(some);
-Console.WriteLine(a);
-Console.WriteLine(b);
-
-(string, string)[] tuples = new[]
-{
-    ("a", "b"),
-    ("c", "d"),
-};
-
-foreach ((string first, string second) in tuples)
-{
-    Console.WriteLine(first);
-    Console.WriteLine(second);
-}
+Menu();
