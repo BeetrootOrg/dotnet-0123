8a3c694076c110e10fc0940e9a99ec44bb85201c
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-02-17 19:18:27 +0200

    exit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿void Menu()
+{
+    Console.Clear();
+
+    Console.WriteLine("Meeting booker");
+    Console.WriteLine("");
+    Console.WriteLine("1, Create a meeting");
+    Console.WriteLine("0, Exit");
+
+    var key = Console.ReadKey();
+    if (key.Key == ConsoleKey.D0)
+    {
+        Exit();
+    }
+}
+
+static void Exit()
+{
+    Environment.Exit(0);
+}
+
+while (true)
+{
+    Menu();
+}
