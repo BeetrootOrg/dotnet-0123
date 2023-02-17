c081d5cb1ef3d4b5c33305f3b198c5477d29246f
igorbelovash <belovash2002@gmail.com>
2023-02-17 09:35:10 +0200

    classwork

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,242 +1,60 @@
-﻿int [] nums1 = { 1, 2, 3};
-int [] nums2 = new[] { 1, 2, 3};
-int [] nums3 = new int[] { 1, 2, 3};
-int [] nums4 = new int[3] { 1, 2, 3};
-int [] nums5 = new int [5];
+﻿char c1 = 'c';
+char c2 = (char)99;
+char c3 = '\u0063';
 
-System.Console.WriteLine(nums1);
-System.Console.WriteLine(nums2);
-System.Console.WriteLine(nums3);
-System.Console.WriteLine(nums4);
-System.Console.WriteLine(nums5);
+Console.WriteLine(c1);
+Console.WriteLine(c2);
+Console.WriteLine(c3);
 
- static void WriteLineArray(int[] arr)
+void DescribeChar( char c)
 {
-    for (int i =0; i < arr.Length; i++)
-    {
-        System.Console.WriteLine(arr[i]);
-    }
+    Console.WriteLine($"Is ASCII = {char.IsAscii(c)}");
+    Console.WriteLine($"Is Control = {char.IsControl(c)}");
+    Console.WriteLine($"Is Digit = {char.IsDigit(c)}");
+    Console.WriteLine($"Is Letter = {char.IsLetter(c)}");
+    Console.WriteLine($"Is Lower = {char.IsLower(c)}");
+    Console.WriteLine($"Is Upper = {char.IsUpper(c)}");
+    Console.WriteLine($"Is Number = {char.IsNumber(c)}");
+    Console.WriteLine($"Is Punctuation = {char.IsPunctuation(c)}");
+    Console.WriteLine($"Is WhiteSpace = {char.IsWhiteSpace(c)}");
 }
-WriteLineArray(nums1);
+Console.WriteLine("Symbol 'c'");
+DescribeChar('c');
 
-static void WriteLineArrayForeach(int[] arr)
-{
-    foreach(int item in arr)
-    {
-        System.Console.WriteLine(item);
-    }
-}
+System.Console.WriteLine($"C greater than c = {'C' > 'c'}");
 
-UpdateElementAt(nums1, 1, 42);
-WriteLineArrayForeach(nums1);
+string s1 = "Hello ,world";
+int age = 26;
+string s2 = "My age is " + age;
+string s3 = $"My age is {age}";
+string s4 = string.Format("My age is {0}", age);
+string s5 = string.Format("My age is {0} and my name is {1}", age, "Dima");
 
-void UpdateElementAt(int[] arr, int index, int newValue)
-{
-    arr[index] = newValue;
-}
-UpdateElementAt(nums1, 1, 42);
-WriteLineArray(nums1);
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
+Console.WriteLine(s4);
+Console.WriteLine(s5);
 
-static void UpateArrayRef( ref int[] arr)
-{
-    arr = new[] { 5, 6, 7, 8};
-}
-UpateArrayRef(ref nums1);
-WriteLineArray(nums1);
+Console.WriteLine($"Contains 'W' = {s1.Contains('W')}");
+Console.WriteLine($"Contains 'World' = {s1.Contains("World")}");
+Console.WriteLine($"Contains 'World' = {s1.Contains("World")}");
+Console.WriteLine($"Contains 'world' OrdinalIgnoreCase = {s1.Contains("world", StringComparison.OrdinalIgnoreCase )}");
 
-int[] nullArr = null;
-if (nullArr != null)
-{
-    System.Console.WriteLine(nullArr.Length);
-}
-UpateArrayRef(ref nullArr);
-WriteLineArrayForeach(nullArr);
+Console.WriteLine($"Helo != World = {"Hello" != "World"}");
+Console.WriteLine($"Helo != Hello = {"Hello" != "Hello"}");
+Console.WriteLine($"Helo != helo = {"Hello" != "helo"}");
 
-System.Console.WriteLine("Range operator 1..3");
-WriteLineArray(nullArr[1..3]); // [1] [2]
+string multiline = "Hello, \nDima";
+string multiline1 = @$"Hello,
+Dima {age}"; 
 
-System.Console.WriteLine("Range operator ..3");
-WriteLineArray(nullArr[..3]); // [0] [1] [2]
+Console.WriteLine(multiline);
+Console.WriteLine(multiline1);
 
-System.Console.WriteLine("Range operator 1..");
-WriteLineArray(nullArr[1..]); // [1] [2] [3]
+System.Console.WriteLine($"s1.Length = {s1.Length}");
 
-int[] subarray = nullArr[1..];
-subarray[0] = 42;
-WriteLineArray(nullArr);
-
-System.Console.WriteLine("Range operator ^3..^1");
-WriteLineArray(nullArr[^3..^1]); // [1] [2]
-
-System.Console.WriteLine("Sum");
-int Sum (int[] arr)
-{
-    int sum = 0;
-    foreach(var item in arr)
-    {
-        sum += item;
-    }
-
-    return sum;
-}
-
-int SumParams (params int[] arr)
-{
-    int sum = 0;
-    foreach(var item in arr)
-    {
-        sum += item;
-    }
-
-    return sum;
-}
-System.Console.WriteLine(Sum(nums2));
-
-System.Console.WriteLine(SumParams(nums2));
-System.Console.WriteLine(SumParams(1, 2, 3));
-System.Console.WriteLine(SumParams());
-
-void ResizeArray( ref int[] arr, int size)
-{
-    if (arr.Length == size)
-    {
-        return;
-    }
-
-    int [] newArr = new int[size];
-    int minLenght = arr.Length < size ? arr.Length : size;
-    for (int i = 0; i < minLenght; i++)
-    {
-            newArr[i] = arr[i];
-    }
-    arr = newArr; 
-}
-
-System.Console.WriteLine("Resize");
-ResizeArray(ref nums3, 4);
-WriteLineArray(nums3);
-
-ResizeArray(ref nums3, 2);
-WriteLineArray(nums3);
-
-Array.Resize(ref nums3, 4);
-WriteLineArray(nums3);
-
- static int[] MulBy2(int[] arr)
-{
-    int[] copy = new int [arr.Length];
-    Array.Copy(arr, copy, arr.Length);
-
-    for (int i = 0; i < copy.Length; i++)
-    {
-        copy[i] *= 2;
-    }
-    return copy;
-}
-
-System.Console.WriteLine("Mul By 2");
-WriteLineArray(MulBy2(nums4));
-
-int[,] matrix = new int[2, 4] {{0, 1, 2, 3}, {4, 5, 6, 7}};
-foreach(var item in matrix)
-{
-    System.Console.WriteLine(item);
-}
-System.Console.WriteLine($"Length = {matrix.Length}");
-System.Console.WriteLine($"GetLength(0) = {matrix.GetLength(0)}");
-System.Console.WriteLine($"GetLength(1) = {matrix.GetLength(1)}");
-for ( int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        System.Console.WriteLine(matrix[i, j]);
-    }
-}
-
-int[] [] jagged = new int[2] []
-{
-    new[] {1, 2, 3},
-    new [] {1, 2}, 
-};
-
-foreach (int[] item in jagged)
-{
-    System.Console.WriteLine("Jagged Array");
-    WriteLineArray(item);
-}
-
-System.Console.WriteLine($"Length = {jagged.Length}");
-
-for (int i = 0; i < jagged.Length; i++)
-{
-    int[] inner = jagged[i];
-    for (int j = 0; j < inner.Length; j++)
-    {
-        System.Console.WriteLine(inner[j]);
-    }
-}
-
-int[] toSort = new [] {-5, 10, 0, 15, -20, 100, 0};
-
-int[] Sort(int[] arr)
-{
-    int[] sorted = new int[arr.Length];
-    Array.Copy(arr, sorted, arr.Length);
-    
-    for (int i = 1; i < arr.Length; i++)
-    {
-        var item = arr[i];
-        var index = i;
-        for (int j = i - 1; j >= 0; j--)
-        {
-            var current = arr[j];
-            if (current > item)
-            {
-                arr[j + 1] = arr[j];
-                index = j;
-            }
-            else 
-            {
-                break;
-            }
-        }
-        arr[index] = item;
-    }
-    return arr;
-}
-
-int [] sorted = new [] { -1, 0, 1, 3};
-int [] random = new [] { -5, 10, 0, 15, -20, 100, 0};
-int [] backsorted = new [] { 3, 1, 0, -1 };
-int [] empty = new int [0];
-
-System.Console.WriteLine("Sort array");
-WriteLineArray(Sort(sorted));
-
-System.Console.WriteLine("Random array");
-WriteLineArray(Sort(random));
-
-System.Console.WriteLine("Backsorted array");
-WriteLineArray(Sort(backsorted));
-
-System.Console.WriteLine("Empty array");
-WriteLineArray(Sort(empty));
-
-int[] Reverse(int[] arr)
-{
-    int[] reversed = new int[arr.Length];
-    Array.Copy(arr, reversed, arr.Length);
-
-    int halfSize = reversed.Length / 2;
-    for (int i = 0; i < halfSize; i++)
-    {
-        int temp = reversed[i];
-        reversed[i] = reversed[^(i + 1)];
-        reversed[^(i + 1)] = temp;
-    }
-    return reversed;
-}
-System.Console.WriteLine("Reverse");
-WriteLineArray(Reverse(new[] {1, 2, 3}));
-System.Console.WriteLine("Reverse");
-WriteLineArray(Reverse(new[] {1, 2, 3, 4}));
+Console.WriteLine($"s1.PadLeft(20) = {s1,20}");
+Console.WriteLine($"{s1,-20} = s1.PadRight(20)");
+Console.WriteLine($"12,4 = {12,4}");
+Console.WriteLine($"12,-4 = {12,-4}");
