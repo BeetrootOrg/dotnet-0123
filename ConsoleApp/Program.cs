06dd06a1f77c36a554ae9a2002626c9a870468b2
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-17 20:03:35 +0100

    fib calc

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int n = 5;
+
+int fib1=1;
+int fib2=1;
+int fib3;
+
+for(int r=1;r<=n;r++)
+{
+    fib3=fib1+fib2;
+    fib1=fib2;
+    fib2=fib3;
+}
+System.Console.WriteLine(fib1);
