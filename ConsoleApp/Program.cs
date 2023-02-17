b5242b34566401216c3eeda02b9b8625a5396be6
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-18 00:02:33 +0200

    Commit x

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,44 +1,13 @@
-﻿int[] nums1 ={1, 2, 3, 4};
-int[] nums2 = new[] {1,2,3,4};
-int[] nums3 = new int[]{1, 2, 3};
-int[] nums4 = new int[3]{1,2,3};
-int[] nums5 = new int[5];
-
-void WriteLineArray (int[] arr)
+﻿void Menu()
 {
-    for (int i = 0; i < arr.Length; i++)
-    {
-        System.Console.WriteLine(arr[i]);
-    }
+    System.Console.Clear();
+
+    System.Console.WriteLine("Meeting Booker");
+    System.Console.WriteLine();
+    System.Console.WriteLine("1. Create a meeting");
+    System.Console.WriteLine("0. Exit");
+
 }
 
-WriteLineArray(nums1);
+Menu();
 
-void UpdateElementAt (int[] arr, int index, int newValue)
-{
-    arr[index] = newValue;
-} 
-
-static void UpdateArray (int[] arr)
-{
-    arr = new[] {5, 6, 7, 8};
-}
-
-UpdateElementAt (nums1, 1, 0);
-WriteLineArray(nums1);
-
-UpdateArray(nums1);
-WriteLineArray(nums1);
-
-static void UpdateArrayRef (ref int[] arr)
-{
-    arr = new[] {5, 6, 7, 8};
-}
-
-UpdateArrayRef(ref nums1);
-WriteLineArray(nums1);
-
-// static void WriteLineForeach (int[] arr)
-// {
-//     foreach
-// }
