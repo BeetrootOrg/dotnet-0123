aa2ed96c2957b6f258d592c0cf110b2d233c534e
coldmayday <mariana.panibudlaska@gmail.com>
2023-02-19 00:48:45 +0200

    Clear

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -29,56 +29,6 @@ void WriteLineArray(int[] arr)
     }
 }
 
-int MinOfArray(int[] arr, out int index)
-{
-    int minOfArray = arr[0];
-    index = 0;
-    for (int i = 1; i < arr.Length; i++)
-    {
-        if (arr[i] < minOfArray)
-        {
-            minOfArray = arr[i];
-            index = i;
-        }
-    }
-
-    return minOfArray;
-}
-
-int[] BubbleSort(int[] arr)
-{
-    int[] copy = CreateArrayCopy(arr, out int length);
-
-    for (int i = 0; i < length; i++)
-    {
-        for (int j = 0; j < length - i - 1; j++)
-        {
-            if (copy[j] > copy[j + 1])
-            {
-                var current = copy[j];
-                copy[j] = copy[j + 1];
-                copy[j + 1] = current;
-            }
-        }
-    }
-
-    return copy;
-}
-
-
-int[] SelectionSort(int[] arr)
-{
-    int[] copy = CreateArrayCopy(arr, out int length);
-
-    for (int i = 0; i < length; i++)
-    {
-        int minMember = MinOfArray(copy[i..], out int indexMin);
-        SwapVar(ref copy[i], ref copy[indexMin + i]);
-    }
-
-    return copy;
-}
-
 int Partition(int[] arr, int low, int high)
 {
    int[] copy = CreateArrayCopy(arr, out int length);
@@ -125,24 +75,6 @@ int[] QuickSort(int[] arr)
 int[] random = new int[] { 6, 3, 2, 4, 1 };
 int[] sorted = new int[] { -7, 0, 5, 7 };
 int[] backsorted = new int[] { 7, 5, 0, -7 };
-
-System.Console.WriteLine("Random Array - BubbleSort");
-WriteLineArray(BubbleSort(random));
-
-System.Console.WriteLine("Sorted Array - BubbleSort");
-WriteLineArray(BubbleSort(sorted));
-
-System.Console.WriteLine("BackSorted Array - BubbleSort");
-WriteLineArray(BubbleSort(backsorted));
-
-System.Console.WriteLine("Random Array - SelectionSort");
-WriteLineArray(SelectionSort(random));
-
-System.Console.WriteLine("Sorted Array - SelectionSort");
-WriteLineArray(SelectionSort(sorted));
-
-System.Console.WriteLine("BackSorted Array - SelectionSort");
-WriteLineArray(SelectionSort(backsorted));
 
 System.Console.WriteLine("Random Array - QuickSort");
 WriteLineArray(QuickSort(random));
