e5f09c5cf36b8ed1050aa91017f117ca562893c2
igorbelovash <belovash2002@gmail.com>
2023-02-18 16:38:48 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,40 @@
 ﻿
  
+using System.Text;
+
+/*Console.WriteLine("Enter the first word");
+string s1 = Console.ReadLine();
+
+Console.WriteLine("Enter the second word");
+string s2 = Console.ReadLine();
+
+bool Compare(string s1, string s2)
+{
+    for( int i = 0; s1.Length < i; i++)
+    {
+        if( s1[i] != s2[i])
+        {
+            Console.WriteLine("strings are not equal");
+            return false;
+        }
+    }
+    if(s1.Length != s2.Length)
+    {
+        Console.WriteLine("strings are not equal");
+        return false;
+    }
+    Console.WriteLine("strings are equal");
+    return true;
+}
+Compare(s1, s2);*/
+
+Console.Clear();
+
+Console.WriteLine("Enter the word");
+string s1 = Console.ReadLine();
+
+void Analyze(string s1)
+{
+    
+}
+
