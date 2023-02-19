e3588001d23612d1912a6f6ad0809ad5e3f81127
Vlada <vlada.lesnaya@gmail.com>
2023-02-19 02:07:57 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,30 +1,27 @@
-﻿int Max(int a, int b, int c = Int32.MinValue, int d = Int32.MinValue)
+﻿int[] BubbleSort(int[] arr)
 {
-    int max1 = Math.Max(a, b);
-    int max2 = Math.Max(c, d);
-    return Math.Max(max1, max2);
-}
-
-int Min(int a, int b, int c = Int32.MaxValue, int d = Int32.MaxValue)
-{
-    int min1 = Math.Min(a, b);
-    int min2 = Math.Min(c, d);
-    return Math.Min(min1, min2);
-}
-
-bool TrySumIfOdd(int a, int b, out int result)
-
-{
-    result = a + b;
-    return (a + b) % 2 != 0;
-}
-
-void Repeat(string str, int n)
-{
-    Console.Write(str);
-    if (n > 1)
+    int[] copy = new int[arr.Length];
+    Array.Copy(arr, copy, arr.Length);
+    int temp;
+    for (int i = 0; i < arr.Length; i++)
     {
-        Repeat(str, n - 1);
+        for (int j = 0; j < arr.Length - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
+
+    return arr;
 }
 
+int[] forSort = new[] { 3, 6, 4, 8, 1 };
+BubbleSort(forSort);
+foreach (var item in forSort)
+{
+    Console.WriteLine(item);
+}
