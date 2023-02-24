bec5be325da04d7e57c52dafb159107afe440c9e
excellent <flarpzz08@gmail.com>
2023-02-24 12:26:30 +0100

    initial commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] unsorted = new int[] { 5, 1, 4, 2, 8 };
+
+int[] BubbleSort(int[] array)
+{
+    if (array == null)
+    {
+        throw new ArgumentNullException();
+    }
+    if (array.Length == 0 || array.Length == 1)
+    {
+        return array;
+    }
+
+    int[] result = new int[array.Length];
+    Array.Copy(array, result, result.Length);
+
+    bool isSorted = false;
+    while (!isSorted)
+    {
+        isSorted = true;
+        for (int i = 0; i < result.Length - 1; i++)
+        {
+            if (result[i] > result[i + 1])
+            {
+                int temp = result[i];
+                result[i] = result[i + 1];
+                result[i + 1] = temp;
+                isSorted = false;
+            }
+        }
+    }
+    return result;
+}
+
+var sorted = BubbleSort(unsorted);
+Console.WriteLine();
+
