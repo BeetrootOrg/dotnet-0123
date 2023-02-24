bda79cbe71854be68205a63bdf640a982def9431
misikdmytro <misik.dmitriy@gmail.com>
2023-02-24 19:54:29 +0200

    record

---
+++ ConsoleApp/Person.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp
+{
+    public record Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,3 +17,21 @@ Console.WriteLine(example.GetFullInformation());
 
 // COMPILATION ERROR BELOW
 // SoftwareEngineer.Example = new SoftwareEngineer("A", "B", "ab", "ab", Positions.Architect, 100000);
+
+int[] a = new[] { 1, 2, 3 };
+int[] b = new[] { 1, 2, 3 };
+
+Console.WriteLine(a == b);
+
+SoftwareEngineer engineer1 = new("John", "Smith", "jsmith", "jsmith@gmail.com",
+    Positions.SoftwareEngineer, 50000);
+
+SoftwareEngineer engineer2 = new("John", "Smith", "jsmith", "jsmith@gmail.com",
+    Positions.SoftwareEngineer, 50000);
+
+Console.WriteLine(engineer1 == engineer2);
+
+Person person1 = new() { FirstName = "John", LastName = "Smith", Age = 30 };
+Person person2 = new() { FirstName = "John", LastName = "Smith", Age = 30 };
+
+Console.WriteLine(person1 == person2);
