18493bb4f13318657004e16d157ae7015213bcf6
igorbelovash <belovash2002@gmail.com>
2023-02-27 09:38:07 +0200

    classwork

--- ConsoleApp/Person.cs
+++ ConsoleApp/Person.cs
@@ -2,7 +2,7 @@ namespace ConsoleApp
 {
      public class Person
     {
-        public int Counter { get; set; }
+        public ChangesTracker Tracker { get; set; } = new ChangesTracker();
         private string _firstName;
         private string _lastName;
         private int _age;
@@ -14,7 +14,7 @@ namespace ConsoleApp
              }
              set
              {
-                ++Counter;
+                Tracker.IncChanges();
                 _firstName = value;
              }
         }
@@ -27,7 +27,7 @@ namespace ConsoleApp
              }
              set
              {
-                ++Counter;
+                Tracker.IncChanges();
                 _lastName = value;
              }
         }
@@ -40,7 +40,7 @@ namespace ConsoleApp
              }
              set
              {
-                ++Counter;
+                Tracker.IncChanges();
                 _age = value;
              }
         }
@@ -49,6 +49,7 @@ namespace ConsoleApp
 
         public Person(string firstName, string lastName, int age)
         {
+            Tracker = new ChangesTracker();
             _firstName = firstName;
             _lastName = lastName;
             _age = age;
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,16 +14,16 @@ Console.WriteLine(person2.LastName);
 Console.WriteLine(person2.FullName);
 
 Person person3 = new Person("A", "B", 27);
-Console.WriteLine($"Changes: {person3.Counter}");
+Console.WriteLine($"Changes: {person3.Tracker.Changes}");
 
 person3.FirstName = "B";
-Console.WriteLine($"Changes: {person3.Counter}");
+Console.WriteLine($"Changes: {person3.Tracker.Changes}");
 
 person3.LastName = "A";
-Console.WriteLine($"Changes: {person3.Counter}");
+Console.WriteLine($"Changes: {person3.Tracker.Changes}");
 
 person3.Age = 43;
-Console.WriteLine($"Changes: {person3.Counter}");
+Console.WriteLine($"Changes: {person3.Tracker.Changes}");
 
 Contact contact1 = new Contact//or - new()
 {
@@ -38,3 +38,40 @@ Contact contact2 = new()
 };
 
 PhoneBook phoneBook = new(contact1, contact2);
+phoneBook.Add(new Contact
+{
+    Person = person3,
+    PhoneNumber = "-+"
+});
+
+Console.WriteLine(phoneBook.Contacts.Length);
+
+User user1 = new(42, "user");
+
+User user2 = new()
+{
+    Username = "user1",
+    Id = 43
+};
+Console.WriteLine(user1.Id);
+Console.WriteLine(user1.Username);
+
+Console.WriteLine(user2.Id);
+Console.WriteLine(user2.Username);
+
+ChangesTracker c1 = new ChangesTracker();
+ChangesTracker c2 = new ChangesTracker();
+
+c1.IncChanges();
+c1.IncChanges();
+c1.IncChanges();
+
+Console.WriteLine(c1.Changes);
+Console.WriteLine(c2.Changes);
+
+Console.WriteLine(ChangesTracker.GlobalChanges);
+
+ChangesTracker.ResetGlobalCounter();
+
+Console.WriteLine(ChangesTracker.GlobalChanges);
+
