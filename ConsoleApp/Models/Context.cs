104e440716eadc470142299d6ee0f20d458a3886
Andrey Tzarkov <admyndc@gmail.com>
2023-02-28 00:45:05 +0200

    домашка

---
+++ ConsoleApp/Models/Context.cs
@@ -0,0 +1,39 @@
+namespace ConsoleApp.Models
+{
+    public class Context
+    {
+        private protected List<Record> Records = new List<Record>();
+        public decimal Balance { get { return Records.Sum(x => x.Summ); } }
+        public List<string> Operations { get { return Records.Select(x => x.ToString()).ToList(); } }
+
+        public void Deposit(decimal summ)
+        {
+            Records.Add(new Record() { Summ = summ, Id = Records.Count });
+        }
+
+        public void Withdraw(decimal summ)
+        {
+            Records.Add(new Record() { Summ = -summ, Id = Records.Count });
+        }
+        public Context()
+        {
+        }
+        // summ - сума початкового депозита
+        public Context(decimal summ)
+        {
+            Deposit(summ);
+        }
+
+        private protected class Record
+        {
+            public int Id { get; set; } = 0;
+            public DateTime DateTime { get; set; } = DateTime.Now;
+            public decimal Summ { get; set; } = 0;
+
+            public override string ToString()
+            {
+                return $"{Id}| {DateTime}| {Summ}";
+            }
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//-------------28/02/2023------------------
+//---------------homework
+// Create class(es) describing ‘bank account’. It should be possible to perform next operations:
+//    deposit/withdraw money
+//    see balance
+//    see operations history
+// User of created classes should not be able to perform other operations
+using ConsoleApp.Models;
+var context = new Context(100.0M);
+context.Deposit(300.0M);
+context.Deposit(300.0M);
+context.Withdraw(100.0M);
+context.Withdraw(100.0M);
+context.Withdraw(100.0M);
+context.Withdraw(100.0M);
+context.Withdraw(100.0M);
+Console.WriteLine($"Balance: {context.Balance}");
+foreach (var s in context.Operations)
+{
+    Console.WriteLine($"{s}");
+}
