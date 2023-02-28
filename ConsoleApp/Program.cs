22d8f4d729e9ffb946faa3bce37a409f1500ac1b
illia Vyshnevskyi <vishna2003@gmail.com>
2023-02-28 13:18:25 +0100

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿bool IfEqual(string a,string b)
+﻿bool Compare(string a,string b)
 
 {
     if (a.Contains(b))
@@ -12,6 +12,41 @@ string a = "Hello";
 string b = "world";
 string c = "Hello";
 
-System.Console.WriteLine(IfEqual(a,b));
+System.Console.WriteLine(Compare(a,b));
 
-System.Console.WriteLine(IfEqual(a,c));
+System.Console.WriteLine(Compare(a,c));
+
+void Analyze(string word)
+{
+    int Digits = 0;
+    int Letters = 0;
+    int Symbols = 0;
+    foreach (char b in word)
+    {
+        if (char.IsLetter(b))
+        {
+            Letters++;
+    
+        }
+        else if (char.IsDigit(b))
+        {
+            Digits++;
+        }
+        else 
+        {
+            Symbols++;
+        }
+    
+    }
+    System.Console.WriteLine(Letters);
+    System.Console.WriteLine(Digits);
+    System.Console.WriteLine(Symbols);
+}
+string word = "hello322$";
+Analyze(word);
+
+
+
+
+    
+
