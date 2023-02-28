8af5fad736f9575d75e7170c7b1f9456bfa40567
Andrey Tzarkov <admyndc@gmail.com>
2023-02-28 02:36:53 +0200

    Homework  inheritance & polymorphism

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ---------- 28/02/2023 ------------------
+// ------------ Homework -------------------
+// Define the next geometric figures: Triangle, Rectangle, Square, Circle. All figures should have next:
+//    Type - should return figure type, e.g. Triangle;
+//    Square - should return figure square;
+//    Perimeter - should return figure perimeter;
+//    SidesNumber - should return the number of sides; (for Circle return -1)
+// Extra:
+//    implement Quadrangle
