0e8b9a6d552519056de06d06d6fc2dd98835a5b9
yuliia-tinana <y.holoto@tinana.de>
2023-03-01 17:21:50 +0100

    homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,46 +1,87 @@
-﻿// Define and call with different parameters next methods:
+﻿// Task:
 //
-// Method that will return max value among all the parameters
-//     … min value …
-// Method TrySumIfOdd that accepts 2 parameters and returns true if sum of numbers between inputs is odd, otherwise false, sum return as out parameter
-//     Overload first two methods with 3 and 4 parameters
+// Implement 3 arr algorithms:
+//
+// Selection
+//     Bubble
+// Insertion
 //
 // Extra:
 //
-// Define and call with different parameters next methods:
+// Define enum arrAlgorithmType with all 3 algorithms types and create single function arr that will accept array and arrAlgorithmType and use passed algorithm to arr array
 //
-// Method Repeat that will accept string X and number N and return X repeated N times (e.g. Repeat(‘str’, 3) returns ‘strstrstr’ = ‘str’ three times)
+// Define enum OrderBy with 2 values: Asc and Desc and update arr method with this parameter that will change arr order
 
-var sourceArray = new int[] { 13, 4, 23, 0, 17, 9, 42, 24 };
-Console.WriteLine($"max value {MaxValue(sourceArray)}");
-Console.WriteLine($"min value {MinValue(sourceArray)}");
-Console.WriteLine($"bool method {TrySumIfOdd(10, 10, out int result)}");
-Console.WriteLine($"repeat string {Repeat("World", 5)}");
+using System;
 
-int MaxValue(int[] sourceArray)
+
+int[] sort = {12, 1243,5,5656,234,56,234,23,5567,976,1,0,4,24,56};
+int[] selection = Selection(sort);
+int[] bublle = Bubble(sort);
+int[] insertion = Insertion(sort);
+Console.WriteLine("Selection Sort");
+selection.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+Console.WriteLine("Selection Sort end");
+
+Console.WriteLine("Bubble Sort");
+bublle.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+Console.WriteLine("Bubble Sort end");
+
+Console.WriteLine("Insertion Sort");
+insertion.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+Console.WriteLine("Insertion Sort end");
+int[]? Selection(int[]? arr)
 {
-    return sourceArray.Max();
-}
+    var arrayLength = arr.Length;
 
-int MinValue(int[] sourceArray)
-{
-    return sourceArray.Min();
-}
-
-bool TrySumIfOdd(int x, int y, out int result)
-{
-    result = (x + y) % 2;
-    if (result != 0)
+    for (int i = 0; i < arrayLength - 1; i++)
     {
-        return true;
+        var smallestVal = i;
+
+        for (int j = i + 1; j < arrayLength; j++)
+        {
+            if (arr[j] < arr[smallestVal])
+            {
+                smallestVal = j;
+            }
+        }
+
+        (arr[smallestVal], arr[i]) = (arr[i], arr[smallestVal]);
     }
-        
-    return false;
+    return arr;
 }
 
-string Repeat(string str, int num)
+int[]? Bubble(int[]? arr)
 {
-    string result = string.Concat(Enumerable.Repeat(str, num));
-    return result;
+    int temp;
+    for (int j = 0; j <= arr.Length - 2; j++) {
+        for (int i = 0; i <= arr.Length - 2; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp= arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
+int[]? Insertion(int[]? arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        var key = arr[i];
+        var flag = 0;
+        for (int j = i - 1; j >= 0 && flag != 1;)
+        {
+            if (key < arr[j])
+            {
+                arr[j + 1] = arr[j];
+                j--;
+                arr[j + 1] = key;
+            }
+            else flag = 1;
+        }
+    }
+    return arr;
+}
