e9190d73509f26ee622a86f6ab6eb09cf35d24f9
Alexander Strekanov <strekanov.alexander@gmail.com>
2023-03-01 14:23:48 +0200

    homework-13

---
+++ ConsoleApp/Shape.cs
@@ -0,0 +1,17 @@
+namespace Geometry
+{
+    public class Shape
+    {
+        protected int _sides { get; init; }   
+        
+        public string Type
+        { 
+            get => this.GetType().ToString().Split('.')[^1];
+        }
+
+        public virtual double Perimeter { get; }
+        public virtual double Area { get; }
+        public virtual int SidesNumber { get; }
+        
+    }
+}
---
+++ ConsoleApp/Square.cs
@@ -0,0 +1,52 @@
+namespace Geometry
+{
+    public class Square : Shape
+    {
+        private double _a;
+        
+        public override double Perimeter
+        {
+            get => _a * 4;
+        }
+
+        public override double Area
+        {
+            get => _a * _a;
+        }  
+
+        public override int SidesNumber
+        {
+            get => _sides;
+        } 
+
+        public Square(double a)
+        {
+            if (a <= 0)
+            {
+                throw new Exception(); 
+            }
+            _a = a;
+            _sides = 4;
+        } 
+
+        public override string ToString()
+        {
+            return $"Type: {Type}; " +
+                $"Side A: {_a}; " +
+                $"Perimeter: {Perimeter}; " +
+                $"Area: {Area}; " + 
+                $"SidesNumber: {_sides}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Square square && _a == square._a;  
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
