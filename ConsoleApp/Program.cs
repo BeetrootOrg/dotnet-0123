9920ca57531017e173b70211e88ea46bdd8a5bd2
IrynaDonienko <114020848+IrynaDonienko@users.noreply.github.com>
2023-03-02 19:35:13 +0100

    changes

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,15 +3,19 @@ using Library;
 Client client1 = new ("Tom", "Hanks", true );
 Client client2 = new ("Robert", "Dauni", false);
 Client client3 = new ("Nicole", "Kidman", true);
-Console.WriteLine(client1);
 
-Author author1 = new Author("Taras", "Shevchenko");
 
-Books book1 = new Books("Love", author1, "novel");
-Books book2 = new Books("Loves", author1, "novels");
+Author author1 = new Author("Vasyl", "Shklar");
+Author author2 = new Author("Taras", "Shevchenko");
+
+
+Book book1 = new Book("Chorniij Voron", author1, "novel");
+Book book2 = new Book("Son", author2, "comedy");
 
 Console.WriteLine(book1.BookId);
 Console.WriteLine(book2.BookId);
 Console.WriteLine(book1.BookId);
-BookAccounting item1 = new BookAccounting(book1, true);
-Record record1 = new Record(book1, client3, , );
+DateTime taking1 = new DateTime(2023, 03, 02);
+DateTime returning1 = new DateTime(2023, 04, 25);
+
+Record record1 = new Record(book1, client3, taking1, returning1);
