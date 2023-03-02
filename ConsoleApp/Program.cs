41700a0ba17653f8f3e78ac92caf6b243b75b350
illia Vyshnevskyi <vishna2003@gmail.com>
2023-03-02 20:55:34 +0100

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,15 @@
 ﻿static void BubbleSort(int[] arr)
 {
     int n = arr.Length;
-    for(int i = 0;i<n-1;i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j = 0; j<n-i-1;j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j]>arr[j+1])
+            if (arr[j] > arr[j + 1])
             {
                 int curr = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j+1] = curr;
+                arr[j + 1] = curr;
             }
         }
     }
@@ -17,13 +17,15 @@
 
 static void WriteLineArray(int[] arr)
 {
-    for (int i = 0;i>arr.Length;i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         System.Console.WriteLine(arr[i]);
     }
 }
-
-int[] arr = {6,1,2,8,5};
+System.Console.WriteLine("arr");
+int[] arr = new[] { 6, 1, 2, 8, 5 };
 WriteLineArray(arr);
+System.Console.WriteLine("sorted arr");
 BubbleSort(arr);
 WriteLineArray(arr);
+
