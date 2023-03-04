b1a3ed8bd9218acb42bfd5d4fc87671d6dea7554
Andrey Tzarkov <admyndc@gmail.com>
2023-03-04 06:14:09 +0200

    Homework 15-Struks

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ---------------04/03/2023-----------------
+// ------------- Homework-------------
+// ------- 15-Struks
+//Create a base Employee class and create different children inheriting it: Manager, Performer, Boss, Assistant, etc. All classes should have next properties/methods:
+//    First/last name;
+//    Responsibilities - an array of responsibilities;
+//    Salary;
+//    Start/End of the working day;
+//    Promotion - the name of the next position;
+//    Promote - should return a new object with the new position.
