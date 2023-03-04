ca7306b0e4fb120284bc8b8b11ffba8e8e076458
Andrey Tzarkov <admyndc@gmail.com>
2023-03-04 07:23:49 +0200

    Reday

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,9 @@
 ﻿// ---------------04/03/2023-----------------
 // ------------- Homework-------------
 // ------- 15-Struks
-//Create a base Employee class and create different children inheriting it: Manager, Performer, Boss, Assistant, etc. All classes should have next properties/methods:
-//    First/last name;
-//    Responsibilities - an array of responsibilities;
-//    Salary;
-//    Start/End of the working day;
-//    Promotion - the name of the next position;
-//    Promote - should return a new object with the new position.
+//Create a struct to work with 3D Point. It should be possible to add points, subtract, 
+//multiply by number ((x, y, z) * n = (n * x, n * y, n * z)), compare two points, 
+//and cast point to the number (the result should be the distance to the point from point (0, 0, 0)).
+using ConsoleApp.Model;
+
+Console.WriteLine("Homework 15-Struks");
