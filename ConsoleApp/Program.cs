508184a36144547b8788574694961ac5df4f1e40
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-06 20:43:45 +0200

    interfaces

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Test _ = new Test();
+// COMPILATION ERROR
+// test.Method();
+
+IA test2 = new Test();
+test2.Method(); // IA.Method
+
+IB test3 = new Test();
+test3.Method(); // IB.Method
+
+internal interface IA
+{
+    void Method();
+}
+
+internal interface IB
+{
+    void Method();
+}
+
+internal class Test : IA, IB
+{
+    void IA.Method()
+    {
+        Console.WriteLine("IA.Method");
+    }
+
+    void IB.Method()
+    {
+        Console.WriteLine("IB.Method");
+    }
+}
