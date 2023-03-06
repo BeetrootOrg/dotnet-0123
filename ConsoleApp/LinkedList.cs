1e5384a6cb67072ea4b49c634073e9697e3c2865
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-06 19:39:29 +0200

    add

--- ConsoleApp/LinkedList.cs
+++ ConsoleApp/LinkedList.cs
@@ -12,27 +12,31 @@ namespace ConsoleApp
 
         public void Add(T item)
         {
+            Element element = new()
+            {
+                Value = item
+            };
+
             if (_head == null)
             {
-                _head = new Element
-                {
-                    Value = item
-                };
+                _head = element;
             }
             else
             {
-                Element element = new()
-                {
-                    Value = item
-                };
-
-                Element last = _head;
-                for (; last.Next != null; last = last.Next)
-                {
-                }
-
+                Element last = Last();
                 last.Next = element;
             }
+        }
+
+        private Element Last()
+        {
+            Element last = _head;
+            while (last.Next != null)
+            {
+                last = last.Next;
+            }
+
+            return last;
         }
     }
 }
