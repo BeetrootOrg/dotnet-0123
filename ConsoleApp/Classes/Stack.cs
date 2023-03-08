2e942ffa328860551db98e6f526cda9536445e3b
Andrey Tzarkov <admyndc@gmail.com>
2023-03-08 20:31:40 +0200

    ready

---
+++ ConsoleApp/Classes/Stack.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.classes
+{
+    public class Stack<T>
+    {
+        public int Count {get=>ToList().Count;}
+
+        private Element? Top;
+
+        public void CopyTo(T[] arr){
+            int length=Count<arr.Length?Count:arr.Length;
+            var source=ToArray();
+            for (int i = 0; i < length; i++)
+            {
+                arr[i]=source[i];
+            }
+        }
+
+        public void Push(T obj)
+        {
+            var element = new Element() { Value = obj };
+            if (Top == null)
+            {
+                Top = element;
+                return;
+            }
+            element.Next = Top;
+            Top = element;
+        }
+
+        public T Peek()
+        {
+            CheckInit();
+            return Top.Value;
+        }
+
+        public void Clear()
+        {
+            Top = null;
+        }
+
+        public T Pop()
+        {
+            CheckInit();
+            T result = Top.Value;
+            Top = Top?.Next;
+            return result;
+        }
+
+        private void CheckInit()
+        {
+            if (Top == null) throw new ArgumentNullException("Sequence not initiated");
+        }
+
+        public override string ToString()
+        {
+            return $"Count {Count}. Values={string.Join(',', ToArray())}.";
+        }
+
+        public T[] ToArray()
+        {
+            return ToList().ToArray();
+        }
+
+        public List<T> ToList()
+        {
+            var list = new List<T>();
+            Element? element = Top;
+            while (element != null)
+            {
+                list.Add(element.Value);
+                element = element.Next;
+            }
+            return list;
+        }
+
+        private class Element
+        {
+            public Element? Next { get; set; }
+            public T Value;
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,3 +7,23 @@
 //    Count - property return number of elements
 //    Peek() - returns top element but doesn’t remove it
 //    CopyTo(arr) - copies stack to array
+using System;
+using ConsoleApp.classes;
+
+var stack=new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
+Console.WriteLine($"Push stack. {stack}");
+Console.WriteLine($"Pop stack. Pop Element value={stack.Pop()}. {stack}");
+Console.WriteLine($"Peek stack. Peek Element value={stack.Peek()}. {stack}");
+var arr=new int[10];
+stack.CopyTo(arr);
+Console.WriteLine($"CopyTo: {string.Join(',', arr)}");
+arr=new int[3];
+stack.CopyTo(arr);
+Console.WriteLine($"CopyTo: {string.Join(',', arr)}");
+stack.Clear();
+Console.WriteLine($"Clear stack. {stack}");
