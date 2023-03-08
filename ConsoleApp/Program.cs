4f23e61d9273c8b36892398924de0a997309ec78
Alexander Strekanov <strekanov.alexander@gmail.com>
2023-03-08 22:36:54 +0200

    homework-16

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ConsoleApp;
+
+Stack<int> stack = new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+
+int pop = stack.Pop();
+Console.WriteLine(pop);
+
+stack.Clear();
+Console.WriteLine(stack.Count);
+
+stack.Push(4);
+stack.Push(5);
+
+int peek = stack.Peek();
+Console.WriteLine(peek);
+
+int[] arr = new int[3];
+stack.CopyTo(arr);
+Console.WriteLine(String.Join(',', arr));
