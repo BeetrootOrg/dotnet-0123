f6d2b3f6c97f647cca509d588e3edfcaec226385
Andrey Tzarkov <admyndc@gmail.com>
2023-03-08 19:14:25 +0200

    Homework 16 Generic classes and methods

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ----- 08.03.2023
+// ----- Homework 16 Generic classes and methods
+// Create generic Stack<T> (what it is) class with next methods:
+//        Push(obj) - adds obj at the top of stack
+//        Pop() - returns top element of stack & removes it
+//        Clear() - clear stack
+//    Count - property return number of elements
+//    Peek() - returns top element but doesn’t remove it
+//    CopyTo(arr) - copies stack to array
