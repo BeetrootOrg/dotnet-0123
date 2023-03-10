a0f96525b5f6509b7b98369f36fb93d5f4f4162b
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-03-10 12:21:52 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp;
+
+Point3D point1 = new Point3D(42, 0, 0.56);
+Console.WriteLine($"point1: {point1}");
+
+Point3D point2 = new Point3D(42, 0, 0.56);
+Console.WriteLine($"point2: {point2}");
+
+Point3D point3 = new Point3D(42, 1, 0.56);
+Console.WriteLine($"point3: {point3}");
+
+Console.WriteLine($"point1==point2: {point1==point2}");
+Console.WriteLine($"point1!=point2: {point1!=point2}");
+Console.WriteLine($"point1==point3: {point1==point3}");
+
+Console.WriteLine($"point1+point2: {point1+point2}");
+Console.WriteLine($"point1-point2: {point1-point2}");
+Console.WriteLine($"point1*3: {point1*3}");
+
+double number = (double)point1;
+Console.WriteLine($"number: {number}");
