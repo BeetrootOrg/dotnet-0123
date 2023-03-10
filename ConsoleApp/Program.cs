4438c51ed8d882496c24f53bd8e7b15526565c9e
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-10 19:30:23 +0200

    list

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿List<int> list = new() { 1, 2, 3 };
+﻿using System;
+using System.Collections.Generic;
+
+List<int> list = new() { 1, 2, 3 };
 Console.WriteLine(list);
 
 static void WriteLineElements<T>(IEnumerable<T> collection)
@@ -25,3 +28,35 @@ List<int> list2 = new(10);
 Console.WriteLine("NEW LIST");
 Console.WriteLine($"Count = {list2.Count}");
 Console.WriteLine($"Capacity = {list2.Capacity}");
+
+list.AddRange(new[] { 6, 7, 8 });
+Console.WriteLine("AFTER ADD");
+WriteLineElements(list);
+
+list.Clear();
+Console.WriteLine("AFTER CLEAR");
+WriteLineElements(list);
+Console.WriteLine($"Count = {list.Count}");
+Console.WriteLine($"Capacity = {list.Capacity}");
+
+list.Add(1);
+Console.WriteLine($"CONTAINS 1 = {list.Contains(1)}");
+Console.WriteLine($"CONTAINS 2 = {list.Contains(2)}");
+
+List<int> l1 = new() { 1, 2, 3 };
+List<int> l2 = new() { 1, 2, 3 };
+
+Console.WriteLine($"l1.Equals(l2) = {l1.Equals(l2)}");
+
+list.Add(42);
+list.Add(100);
+Console.WriteLine($"INDEX OF 42 = {list.IndexOf(42)}");
+Console.WriteLine($"INDEX OF 43 = {list.IndexOf(43)}");
+
+list.Insert(1, 43);
+WriteLineElements(list);
+
+list.Remove(43);
+list.RemoveAt(1);
+
+WriteLineElements(list);
