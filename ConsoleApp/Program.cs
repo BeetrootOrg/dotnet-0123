5afbe4e9d1b4cc7a20919d6a1f0fb0a04cdea877
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-10 19:21:53 +0200

    capacity vs count

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,3 +8,20 @@ static void WriteLineElements<T>(IEnumerable<T> collection)
 
 WriteLineElements(list);
 WriteLineElements(new[] { 4, 5, 6 });
+
+list.Add(4);
+Console.WriteLine("AFTER ADD");
+WriteLineElements(list);
+Console.WriteLine($"Count = {list.Count}");
+Console.WriteLine($"Capacity = {list.Capacity}");
+
+list.Add(5);
+Console.WriteLine("AFTER ADD");
+WriteLineElements(list);
+Console.WriteLine($"Count = {list.Count}");
+Console.WriteLine($"Capacity = {list.Capacity}");
+
+List<int> list2 = new(10);
+Console.WriteLine("NEW LIST");
+Console.WriteLine($"Count = {list2.Count}");
+Console.WriteLine($"Capacity = {list2.Capacity}");
