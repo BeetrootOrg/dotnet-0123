97c84e7c9e20b47a8aa2332e1f01f5b5c5215186
illia Vyshnevskyi <vishna2003@gmail.com>
2023-03-11 19:21:39 +0100

    save

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Menu()
+{
+    Console.Clear();
+    System.Console.WriteLine("Meeting Booker");
+    System.Console.WriteLine();
+    System.Console.WriteLine("1.Create meeting");
+    System.Console.WriteLine("0.Exit");
+}
+Menu();
