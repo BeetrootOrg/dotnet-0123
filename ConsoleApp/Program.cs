0f04ea5e7839219be3bab9d48e333cf8e8e01aba
illia Vyshnevskyi <vishna2003@gmail.com>
2023-03-11 19:48:58 +0100

    save

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,10 +12,37 @@
     {
         Exit();
     }
+    else if (key.Key == ConsoleKey.D1)
+    {
+        CreateMeeting();
+    }
 }
 static void Exit()
 {
     Environment.Exit(0);
+}
+
+static void CreateMeeting()
+{
+    Console.Clear();
+
+    System.Console.WriteLine("Enter Meeting Name:");
+    string meetingName = Console.ReadLine();
+
+    System.Console.WriteLine("Enter Meeting start:");
+    string meetingStartInput = Console.ReadLine();
+    DateTime meetingStart = DateTime.Parse(meetingStartInput);
+
+    System.Console.WriteLine("Enter meeting duration");
+    string meetingDurationInput = Console.ReadLine();
+    int meetingDuration = int.Parse(meetingDurationInput);
+    
+    System.Console.WriteLine("Room Name");
+    string roomName = Console.ReadLine();
+
+System.Console.WriteLine($"{meetingName}{meetingStart}{meetingDuration}{roomName}");
+
+
 }
 while(true)
 {
