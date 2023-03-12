c7fa71f687d88138f40fe93083e3b28ebc560b16
illia Vyshnevskyi <vishna2003@gmail.com>
2023-03-13 00:46:10 +0100

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿(string,string,string)[] contacts = new (string,string,string)[0];
+﻿(string, string, string)[] contacts = new (string, string, string)[0];
 
 
 void PhoneBook()
@@ -22,7 +22,10 @@ void PhoneBook()
     {
         ShowContacts();
     }
-
+    else if (key.Key == ConsoleKey.D3)
+    {
+        FindContact();
+    }
 }
 static void Exit()
 {
@@ -43,25 +46,48 @@ void AddNewContact()
     string PhoneNumber = Console.ReadLine();
 
 
-Array.Resize(ref contacts,contacts.Length +1);
-contacts[^1] = (FirstName, LastName, PhoneNumber);
+    Array.Resize(ref contacts, contacts.Length + 1);
+    contacts[^1] = (FirstName, LastName, PhoneNumber);
 
     System.Console.WriteLine("Contact succesfully added");
-    System.Console.WriteLine($"To continue press ENTER...");
+    System.Console.WriteLine($"Press ENTER to continue...");
     Console.ReadLine();
 }
- void ShowContacts()
+void ShowContacts()
 {
     Console.Clear();
-    System.Console.WriteLine($"{"Name",-20}{"Start",-20}{"End",-20}{"Room",-20}");
+    System.Console.WriteLine($"{"First Name",-20}{"Last Name",-20}{"Phone Number",-20}");
     foreach (var (FirstName, LastName, PhoneNumber) in contacts)
     {
         System.Console.WriteLine();
-        System.Console.WriteLine($"{FirstName ,-20},{LastName,-20}{PhoneNumber,-20}");
+        System.Console.WriteLine($"{FirstName,-20}{LastName,-20}{PhoneNumber,-20}");
 
     }
-}
+    System.Console.WriteLine("Press ENTER to countinue...");
+    Console.ReadLine();
 
+}
+void FindContact()
+{
+    Console.Clear();
+    System.Console.WriteLine("Enter First/Last name or phone number:");
+    string info = Console.ReadLine();
+    foreach (var (FirstName, LastName, PhoneNumber) in contacts)
+    {
+        if (FirstName.Contains(info) || LastName.Contains(info) || PhoneNumber.Contains(info))
+        {
+            System.Console.WriteLine("What we found:");
+            System.Console.WriteLine($"{FirstName,-20}{LastName,-20}{PhoneNumber,-20}");
+        }
+        else
+        {
+            System.Console.WriteLine($"There is no contact {info}");
+        }
+    }
+    System.Console.WriteLine("Press ENTER to countinue...");
+    Console.ReadLine();
+
+}
 while (true)
 {
     PhoneBook();
