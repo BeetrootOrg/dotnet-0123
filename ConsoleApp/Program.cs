39fb45d4478ef94359ce17c236e1f08afd103116
IrynaDonienko <114020848+IrynaDonienko@users.noreply.github.com>
2023-03-13 23:01:12 +0100

    stack

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using GenericStack;
+
+Stack <string> first = new Stack<string>();
+Console.WriteLine(first.Count);
+first.Puch("hello");
+first.Puch("hello1");
+first.Puch("hello2");
+first.Puch("hello3");
+Console.WriteLine(first.Count);
+first.Pop();
+Console.WriteLine(first.Peek());
+Console.WriteLine(first.Count);
+first.Clear();
+first.Pop();
+Console.WriteLine(first.Count);
+
