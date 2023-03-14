b5fab1ac75bf9dd21ef52342b4ab6064ce0f1d23
Andrey Tzarkov <admyndc@gmail.com>
2023-03-14 21:49:26 +0200

    Ready

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//============14/03/2023=============
+// Homework Події, делегати і лямбди
+// 1. Create a program to ‘vote’ for anything. 
+// Via the console interface users will create a ‘vote topic’ with options. 
+// Voters will vote via console interface as well. Users can see voting results via console interface.
+// 2. Extra Snake game
+
+using ConsoleApp.Menus;
+
+MenuHelper.MenuTopAction ();
