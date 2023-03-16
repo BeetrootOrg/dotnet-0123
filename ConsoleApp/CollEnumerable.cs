daac934915e642f1452f273f12766ab9b66defa4
Alexander Strekanov <strekanov.alexander@gmail.com>
2023-03-16 09:35:56 +0200

    added: CollEnumerable class

---
+++ ConsoleApp/CollEnumerable.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+
+namespace ConsoleApp
+{
+    public class CollEnumerable<T> : IEnumerable<T>
+    {
+        private IEnumerable<T> _collection;
+        private Predicate<T> _predicate;
+
+        public CollEnumerable(IEnumerable<T> collection, Predicate<T> predicate)
+        {
+            _collection = collection;
+            _predicate = predicate;
+        }
+
+        private class CollEnumerator : IEnumerator<T>
+        {
+            private IEnumerator<T> _enumerator;
+            private Predicate<T> _predicate;
+
+            public CollEnumerator(IEnumerator<T> enumerator, Predicate<T> predicate)
+            {
+                _enumerator = enumerator;
+                _predicate = predicate;
+            }
+
+            public T Current { get; private set; }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+                _enumerator.Dispose();
+            }
+
+            public bool MoveNext()
+            {
+                while(_enumerator.MoveNext())
+                {
+                    if (_predicate(_enumerator.Current))
+                    {
+                        Current = _enumerator.Current;
+                        return true; 
+                    }
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                _enumerator.Reset();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CollEnumerator(_collection.GetEnumerator(), _predicate);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
