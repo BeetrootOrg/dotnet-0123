1153c3464a10eedc92797bf9c57a2e83546ac76b
Dmytro Misik <d.misik@draftkings.com>
2023-03-16 15:37:51 +0200

    LINQ

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using ConsoleApp;
+
+using Newtonsoft.Json;
+
+IEnumerable<Person> persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+Console.WriteLine(string.Join(Environment.NewLine, persons));
