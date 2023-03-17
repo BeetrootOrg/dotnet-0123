4f61d6036451966cf4edb93c722e1c036f654699
Andrey Tzarkov <admyndc@gmail.com>
2023-03-18 00:31:25 +0200

    start

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,2 @@
 ﻿
-//============18/03/2023=============
-// Homework 19-Методи розширення
-// GroupBy(enumerable, func) extension to a collection that will return a dictionary 
-// of grouped items by func. E.g. GroupBy([{a: 1, b: 2}, {a: 1, b: 3}, 
-// {a: 2, b: 2}, item => item.a] => {1: [{a: 1, b: 2}, {a: 1, b: 3}], 2: [{a: 2, b: 2}]}
 
