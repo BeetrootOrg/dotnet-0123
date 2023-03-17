fcc4a6bfea36c02828cafc455cb5d4ceab721206
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-17 19:26:29 +0200

    male/female count

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,3 +10,8 @@ using Newtonsoft.Json;
 IEnumerable<Person> persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
 
 Console.WriteLine($"People count = {persons.Count()}");
+
+Func<Person, bool> malePredicate = (Person p) => p.Gender is Gender.Male;
+
+Console.WriteLine($"Male count = {persons.Count(malePredicate)}");
+Console.WriteLine($"Female count = {persons.Count(p => p.Gender is Gender.Female)}");
