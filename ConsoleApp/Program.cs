e49443c57802bfb40796f1ace8980ef1ed25efcc
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-17 19:31:54 +0200

    min/max

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,3 +15,16 @@ Func<Person, bool> malePredicate = (Person p) => p.Gender is Gender.Male;
 
 Console.WriteLine($"Male count = {persons.Count(malePredicate)}");
 Console.WriteLine($"Female count = {persons.Count(p => p.Gender is Gender.Female)}");
+
+int youngestAge = persons.Min(p => p.Age);
+Person youngest = persons.MinBy(p => p.Age);
+
+Console.WriteLine($"Youngest age = {youngestAge}");
+Console.WriteLine($"Youngest person = {youngest}");
+
+
+int oldestAge = persons.Max(p => p.Age);
+Person oldest = persons.MaxBy(p => p.Age);
+
+Console.WriteLine($"Oldest age = {oldestAge}");
+Console.WriteLine($"Oldest person = {oldest}");
