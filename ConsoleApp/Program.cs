57e9d4cd8a30b584f1406125bbe2531ef969cb02
igorbelovash <belovash2002@gmail.com>
2023-03-18 17:15:05 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,14 +1,40 @@
-﻿using ConsoleApp;
+﻿using System;
 
-Point p1 = new(2, 3, 4);
-Point p2 = new(2, 3, 4);
-Console.WriteLine(p1);
-Console.WriteLine(p1.Equals(p2));
-Console.WriteLine(p1 + p2);
-Console.WriteLine(p1 - p2);
-Console.WriteLine(p1 * 5);
+using ConsoleApp;
 
-// Console.WriteLine(p1 == p2);
-// Console.WriteLine(p1 != p2);
 
-Console.WriteLine(Point.Const(p2));
+Stack<int> stack = new();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+Console.WriteLine(stack);
+
+stack.Pop();
+Console.WriteLine(stack);
+
+stack.Clear();
+Console.WriteLine(stack);
+
+stack.Push(10);
+stack.Push(11);
+stack.Push(12);
+
+Console.WriteLine(stack.Peek());
+
+int[] arr1 = new int[2];
+int[] arr2 = new int[3];
+int[] arr3 = new int[4];
+
+stack.CopyTo(arr1);
+stack.CopyTo(arr2);
+stack.CopyTo(arr3);
+
+static void WriteLineArray<T>(T[] arr)
+{
+    Console.WriteLine(string.Join(", ", arr));
+}
+
+WriteLineArray(arr1);
+WriteLineArray(arr2);
+WriteLineArray(arr3);
