65e0301a2ac1440f0f87d0e661bd202ac21ede0f
Andrey Tzarkov <admyndc@gmail.com>
2023-03-18 06:51:12 +0200

    Ready

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿///==========18/03/2023=============
+// ===== Homework= 19-Linq
+// 
+//    find out who is located farthest north/south/west/east using latitude/longitude data
+//    find max and min distance between 2 persons
+//    find 2 persons whos ‘about’ have the most same words
+//    find persons with same friends (compare by friend’s name) 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -8,117 +15,168 @@ using ConsoleApp;
 
 using Newtonsoft.Json;
 
+
 IEnumerable<Person> persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
 
-Console.WriteLine($"People count = {persons.Count()}");
+Console.WriteLine("find out who is located farthest north/south/west/east using latitude/longitude data");
+Console.WriteLine($"{persons.MaxBy(x => x.Longitude)} is located farthest north.");
+Console.WriteLine($"{persons.MinBy(x => x.Longitude)} is located farthest south.");
+Console.WriteLine($"{persons.MaxBy(x => x.Latitude)} is located farthest west.");
+Console.WriteLine($"{persons.MinBy(x => x.Latitude)} is located farthest east.");
 
-Func<Person, bool> malePredicate = (Person p) => p.Gender is Gender.Male;
+Console.WriteLine("find max and min distance between 2 persons");
+var maxdist = persons.Join(persons, p => true, p => true, (p1, p2) => new { First = p1, Second = p2 })
+.Where(group => !ReferenceEquals(group.First, group.Second)).
+Max(x => Math.Sqrt((x.First.Latitude - x.Second.Latitude) * (x.First.Latitude - x.Second.Latitude) +
+(x.First.Longitude - x.Second.Longitude) * (x.First.Longitude - x.Second.Longitude)));
+var mindist = persons.Join(persons, p => true, p => true, (p1, p2) => new { First = p1, Second = p2 }).
+Where(group => !ReferenceEquals(group.First, group.Second)).
+Min(x => Math.Sqrt((x.First.Latitude - x.Second.Latitude) * (x.First.Latitude - x.Second.Latitude) +
+(x.First.Longitude - x.Second.Longitude) * (x.First.Longitude - x.Second.Longitude)));
+Console.WriteLine($"Max distance={maxdist}. Min distance={mindist}");
 
-Console.WriteLine($"Male count = {persons.Count(malePredicate)}");
-Console.WriteLine($"Female count = {persons.Count(p => p.Gender is Gender.Female)}");
-
-int youngestAge = persons.Min(p => p.Age);
-Person youngest = persons.MinBy(p => p.Age);
-
-Console.WriteLine($"Youngest age = {youngestAge}");
-Console.WriteLine($"Youngest person = {youngest}");
-Console.WriteLine($"Youngest person name = {youngest.Name}");
-
-int oldestAge = persons.Max(p => p.Age);
-Person oldest = persons.MaxBy(p => p.Age);
-
-Console.WriteLine($"Oldest age = {oldestAge}");
-Console.WriteLine($"Oldest person = {oldest}");
-Console.WriteLine($"Oldest person name = {oldest.Name}");
-
-double averageAge = persons.Average(p => p.Age);
-Console.WriteLine($"Average age = {averageAge}");
-
-foreach (IGrouping<string, Person> group in persons.GroupBy(p => p.EyeColor))
+Console.WriteLine("find 2 persons whos ‘about’ have the most same words");
+var personsabout = persons.Join(persons, p => true, p => true, (p1, p2) => new { First = p1, Second = p2 }).
+Where(group => !ReferenceEquals(group.First, group.Second)).Select(x =>
+new
 {
-    Console.WriteLine($"There are {group.Count()} with {group.Key} eye color");
-}
+    Per1 = x.First,
+    Per2 = x.Second,
+    Count = x.Second.About.Split(' ').Where(y => x.First.About.Split(' ').Contains(y)).Count()
+}).ToList();
+var maxabout = personsabout.Max(x => x.Count);
+var minabout = personsabout.Min(x => x.Count);
 
-foreach (IGrouping<Gender, Person> group in persons.GroupBy(p => p.Gender))
+foreach (var x in personsabout.Where(x => x.Count == minabout)) Console.WriteLine($"{x.Per1.Name} and  {x.Per2.Name}");
+foreach (var x in personsabout.Where(x => x.Count == maxabout)) Console.WriteLine($"{x.Per1.Name} and  {x.Per2.Name}");
+
+Console.WriteLine("find persons with same friends (compare by friend’s name)");
+
+var personsamefrends = persons.Join(persons, p => true, p => true, (p1, p2) => new { First = p1, Second = p2 }).
+Where(group => !ReferenceEquals(group.First, group.Second)).Select(x =>
+new
 {
-    string gender = group.Key.ToString().ToLower(CultureInfo.CurrentCulture);
-    Console.WriteLine($"There are {group.Count()} {gender}s");
-}
+    Per1 = x.First,
+    Per2 = x.Second,
+    frends1 = x.First.Friends.Select(x => x.Name),
+    frends2 = x.Second.Friends.Select(x => x.Name)
+}).Where(x => x.frends1.SequenceEqual(x.frends2)).ToList();
+foreach (var z in personsamefrends)
+    Console.WriteLine($"{z.Per1.Name} and  {z.Per2.Name}");
 
-IEnumerable<Name> names = persons
-    .Select(p =>
-    {
-        string[] splitted = p.Name.Split(' ');
-        return new Name
-        {
-            FirstName = splitted[0],
-            LastName = splitted[1]
-        };
-    })
-    .ToArray();
 
-Console.WriteLine(string.Join(Environment.NewLine, names));
 
-IEnumerable<IGrouping<string, string>> nonUniqueNames = names.Select(n => n.FirstName)
-    .GroupBy(x => x)
-    .Where(group => group.Count() > 1);
 
-foreach (IGrouping<string, string> group in nonUniqueNames)
-{
-    Console.WriteLine($"Name {group.Key} appeared {group.Count()} times");
-}
 
-foreach (var group in persons.Join(
-                                    persons,
-                                    p => true,
-                                    p => true,
-                                    (p1, p2) => new
-                                    {
-                                        First = p1,
-                                        Second = p2
-                                    }
-                                ).Where(group => !ReferenceEquals(group.First, group.Second))
-                                .Where(group => group.First.Company == group.Second.Company))
-{
-    Console.WriteLine($"Potential coleagues are {group.First.Name} and {group.Second.Name}");
-}
+// Console.WriteLine($"People count = {persons.Count()}");
 
-Person theFriendestPerson = persons.MaxBy(p => p.Friends.Length);
-Console.WriteLine($"Person with max friends number is {theFriendestPerson.Name} with {theFriendestPerson.Friends.Length} friends");
+// Func<Person, bool> malePredicate = (Person p) => p.Gender is Gender.Male;
 
-// 1st approach
-IGrouping<int, Person> friendestPeople1 = persons.GroupBy(p => p.Friends.Length)
-    .MaxBy(group => group.Key);
+// Console.WriteLine($"Male count = {persons.Count(malePredicate)}");
+// Console.WriteLine($"Female count = {persons.Count(p => p.Gender is Gender.Female)}");
 
-Console.WriteLine($"People with max friends number are {string.Join(", ", friendestPeople1.Select(p => p.Name))} with {friendestPeople1.Key} friends each");
+// int youngestAge = persons.Min(p => p.Age);
+// Person youngest = persons.MinBy(p => p.Age);
 
-// 2nd approach
-int maxFriendsNumber = persons.Max(p => p.Friends.Length);
-Person[] friendestPeople2 = persons.Where(p => p.Friends.Length == maxFriendsNumber).ToArray();
+// Console.WriteLine($"Youngest age = {youngestAge}");
+// Console.WriteLine($"Youngest person = {youngest}");
+// Console.WriteLine($"Youngest person name = {youngest.Name}");
 
-Console.WriteLine($"There are {friendestPeople2.Length} people with {maxFriendsNumber} of friends");
+// int oldestAge = persons.Max(p => p.Age);
+// Person oldest = persons.MaxBy(p => p.Age);
 
-Console.WriteLine($"Everybody registered in past = {persons.All(p => p.Registered < DateTime.Now)}");
-Console.WriteLine($"Anybody without friend = {persons.Any(p => p.Friends.Length == 0)}");
+// Console.WriteLine($"Oldest age = {oldestAge}");
+// Console.WriteLine($"Oldest person = {oldest}");
+// Console.WriteLine($"Oldest person name = {oldest.Name}");
 
-IEnumerable<Person> distinctByEyeColor = persons.DistinctBy(p => p.EyeColor);
-Console.WriteLine($"{nameof(distinctByEyeColor)}.Count() = {distinctByEyeColor.Count()}");
+// double averageAge = persons.Average(p => p.Age);
+// Console.WriteLine($"Average age = {averageAge}");
 
-Person firstPerson = persons.First();
-Person firstWith4Friends = persons.First(p => p.Friends.Length == 4);
+// foreach (IGrouping<string, Person> group in persons.GroupBy(p => p.EyeColor))
+// {
+//     Console.WriteLine($"There are {group.Count()} with {group.Key} eye color");
+// }
 
-Console.WriteLine($"First = {firstPerson}");
-Console.WriteLine($"First with 4 friend = {firstWith4Friends}");
+// foreach (IGrouping<Gender, Person> group in persons.GroupBy(p => p.Gender))
+// {
+//     string gender = group.Key.ToString().ToLower(CultureInfo.CurrentCulture);
+//     Console.WriteLine($"There are {group.Count()} {gender}s");
+// }
 
-Person firstWith6Friends = persons.FirstOrDefault(p => p.Friends.Length == 6);
-Console.WriteLine($"First with 6 friend = {firstWith6Friends}");
+// IEnumerable<Name> names = persons
+//     .Select(p =>
+//     {
+//         string[] splitted = p.Name.Split(' ');
+//         return new Name
+//         {
+//             FirstName = splitted[0],
+//             LastName = splitted[1]
+//         };
+//     })
+//     .ToArray();
 
-Person personWithExactName = persons.Single(p => p.Name == "Holloway Meyers");
-Console.WriteLine($"Person with exact name = {personWithExactName}");
+// Console.WriteLine(string.Join(Environment.NewLine, names));
 
-Person orderedPerson = persons.OrderBy(x => x.Name).First();
-Console.WriteLine($"First person = {orderedPerson}");
+// IEnumerable<IGrouping<string, string>> nonUniqueNames = names.Select(n => n.FirstName)
+//     .GroupBy(x => x)
+//     .Where(group => group.Count() > 1);
 
-IEnumerable<Friend[]> allFriendsBad = persons.Select(x => x.Friends);
-IEnumerable<Friend> allFriendsGood = persons.SelectMany(x => x.Friends);
+// foreach (IGrouping<string, string> group in nonUniqueNames)
+// {
+//     Console.WriteLine($"Name {group.Key} appeared {group.Count()} times");
+// }
+
+// foreach (var group in persons.Join(
+//                                     persons,
+//                                     p => true,
+//                                     p => true,
+//                                     (p1, p2) => new
+//                                     {
+//                                         First = p1,
+//                                         Second = p2
+//                                     }
+//                                 ).Where(group => !ReferenceEquals(group.First, group.Second))
+//                                 .Where(group => group.First.Company == group.Second.Company))
+// {
+//     Console.WriteLine($"Potential coleagues are {group.First.Name} and {group.Second.Name}");
+// }
+
+// Person theFriendestPerson = persons.MaxBy(p => p.Friends.Length);
+// Console.WriteLine($"Person with max friends number is {theFriendestPerson.Name} with {theFriendestPerson.Friends.Length} friends");
+
+// // 1st approach
+// IGrouping<int, Person> friendestPeople1 = persons.GroupBy(p => p.Friends.Length)
+//     .MaxBy(group => group.Key);
+
+// Console.WriteLine($"People with max friends number are {string.Join(", ", friendestPeople1.Select(p => p.Name))} with {friendestPeople1.Key} friends each");
+
+// // 2nd approach
+// int maxFriendsNumber = persons.Max(p => p.Friends.Length);
+// Person[] friendestPeople2 = persons.Where(p => p.Friends.Length == maxFriendsNumber).ToArray();
+
+// Console.WriteLine($"There are {friendestPeople2.Length} people with {maxFriendsNumber} of friends");
+
+// Console.WriteLine($"Everybody registered in past = {persons.All(p => p.Registered < DateTime.Now)}");
+// Console.WriteLine($"Anybody without friend = {persons.Any(p => p.Friends.Length == 0)}");
+
+// IEnumerable<Person> distinctByEyeColor = persons.DistinctBy(p => p.EyeColor);
+// Console.WriteLine($"{nameof(distinctByEyeColor)}.Count() = {distinctByEyeColor.Count()}");
+
+// Person firstPerson = persons.First();
+// Person firstWith4Friends = persons.First(p => p.Friends.Length == 4);
+
+// Console.WriteLine($"First = {firstPerson}");
+// Console.WriteLine($"First with 4 friend = {firstWith4Friends}");
+
+// Person firstWith6Friends = persons.FirstOrDefault(p => p.Friends.Length == 6);
+// Console.WriteLine($"First with 6 friend = {firstWith6Friends}");
+
+// Person personWithExactName = persons.Single(p => p.Name == "Holloway Meyers");
+// Console.WriteLine($"Person with exact name = {personWithExactName}");
+
+// Person orderedPerson = persons.OrderBy(x => x.Name).First();
+// Console.WriteLine($"First person = {orderedPerson}");
+
+// IEnumerable<Friend[]> allFriendsBad = persons.Select(x => x.Friends);
+// IEnumerable<Friend> allFriendsGood = persons.SelectMany(x => x.Friends);
 
