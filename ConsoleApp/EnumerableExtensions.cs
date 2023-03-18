87db6337e8504a94023efa2e6fe6e39717767aae
Andrey Tzarkov <admyndc@gmail.com>
2023-03-18 03:10:26 +0200

    stupid extensions

--- ConsoleApp/EnumerableExtensions.cs
+++ ConsoleApp/EnumerableExtensions.cs
@@ -5,6 +5,23 @@ namespace ConsoleApp
 {
     public static class EnumerableExtensions
     {
+        public static IEnumerable<GroupEnumerable<TKey, TSourch>> GroupBy<TKey, TSourch>(this IEnumerable<TSourch> sourch, Func<TSourch, TKey> keySelector)
+        {
+            if (sourch==null || keySelector==null) throw new ArgumentNullException("I really want to sleep");
+            IEnumerable<TKey> Keys=sourch.Select<TSourch, TKey>(x=>keySelector.Invoke(x)).Destinc();
+            foreach(TKey key in Keys){
+                yield return new GroupEnumerable<TKey, TSourch>(key, sourch.Where<TSourch>(x=>keySelector.Invoke(x).Equals(key)));
+            }           
+        }
+
+        public static IEnumerable<TSourch> Destinc<TSourch>(this IEnumerable<TSourch> sourch){
+            List<TSourch> list=new();
+            foreach(var item in sourch){
+                if (!list.Contains(item)) list.Add(item);
+            }
+            return list;
+        }
+
         public static IEnumerable<T> Where<T>(this IEnumerable<T> collection, Predicate<T> predicate)
         {
             return new WhereEnumerable<T>(collection, predicate);
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,20 @@
-﻿using System;
+﻿//=============18/03/2023===========
+// Homework 19-Extensions
+
+using System;
 using System.Collections.Generic;
 
 using ConsoleApp;
 
+var list = new List<(string, int)>(){new (){Item1="1",Item2=1}, new (){Item1="1",Item2=2},new (){Item1="1",Item2=3},
+new (){Item1="2",Item2=1},new (){Item1="2",Item2=2},new (){Item1="2",Item2=3},new (){Item1="3",Item2=3},
+new (){Item1="3",Item2=1}};
+var groulist = list.GroupBy(x => x.Item1);
+foreach (var group in groulist)
+{
+    Console.WriteLine($"{group.Key}");
+    foreach (var item in group)
+    {
+        Console.WriteLine($"\t{item.Item1} {item.Item2}");
+    }
+}
