6ebad39aff38a49170b18382392f7f9fb016a0a6
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-20 19:17:21 +0200

    properties

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,20 @@
-﻿int a = 42;
+﻿using System;
+using System.Reflection;
+
+int a = 42;
 Type type = a.GetType();
 
 Console.WriteLine(type.Name);
 Console.WriteLine(type.FullName);
+
+Type testType = typeof(Test);
+foreach (PropertyInfo propertyInfo in testType.GetProperties())
+{
+    Console.WriteLine($"Type {testType.Name} has property {propertyInfo.Name} with type {propertyInfo.PropertyType.Name}");
+}
+
+internal class Test
+{
+    public int Number { get; set; }
+    public string Value { get; set; }
+}
