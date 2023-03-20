fd78e82d835e2e1dc49f1e68f8b4031327f2fbed
yuliia-tinana <y.holoto@tinana.de>
2023-03-20 16:55:13 +0100

    homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,87 +1,114 @@
-﻿// Task:
-//
-// Implement 3 arr algorithms:
-//
-// Selection
-//     Bubble
-// Insertion
-//
-// Extra:
-//
-// Define enum arrAlgorithmType with all 3 algorithms types and create single function arr that will accept array and arrAlgorithmType and use passed algorithm to arr array
-//
-// Define enum OrderBy with 2 values: Asc and Desc and update arr method with this parameter that will change arr order
-
-using System;
+﻿using System.Globalization;
 
 
-int[] sort = {12, 1243,5,5656,234,56,234,23,5567,976,1,0,4,24,56};
-int[] selection = Selection(sort);
-int[] bublle = Bubble(sort);
-int[] insertion = Insertion(sort);
-Console.WriteLine("Selection Sort");
-selection.ToList().ForEach(i => Console.WriteLine(i.ToString()));
-Console.WriteLine("Selection Sort end");
+// Compare that will return true if 2 strings are equal,
+// otherwise false, but do not use build-in method (bool Compare(string, string))
 
-Console.WriteLine("Bubble Sort");
-bublle.ToList().ForEach(i => Console.WriteLine(i.ToString()));
-Console.WriteLine("Bubble Sort end");
 
-Console.WriteLine("Insertion Sort");
-insertion.ToList().ForEach(i => Console.WriteLine(i.ToString()));
-Console.WriteLine("Insertion Sort end");
-int[]? Selection(int[]? arr)
+bool Compare(string a, string b, bool checkStringEqualWithCase = true)
 {
-    var arrayLength = arr.Length;
-
-    for (int i = 0; i < arrayLength - 1; i++)
+    if (checkStringEqualWithCase == false)
     {
-        var smallestVal = i;
-
-        for (int j = i + 1; j < arrayLength; j++)
+        if (a.ToLower(CultureInfo.CurrentCulture)==b.ToLower(CultureInfo.CurrentCulture))
         {
-            if (arr[j] < arr[smallestVal])
-            {
-                smallestVal = j;
-            }
-        }
-
-        (arr[smallestVal], arr[i]) = (arr[i], arr[smallestVal]);
+            return true;
+        } 
     }
-    return arr;
-}
-
-int[]? Bubble(int[]? arr)
-{
-    int temp;
-    for (int j = 0; j <= arr.Length - 2; j++) {
-        for (int i = 0; i <= arr.Length - 2; i++) {
-            if (arr[i] > arr[i + 1]) {
-                temp= arr[i + 1];
-                arr[i + 1] = arr[i];
-                arr[i] = temp;
-            }
-        }
-    }
-    return arr;
-}
-
-int[]? Insertion(int[]? arr)
-{
-    for (int i = 1; i < arr.Length; i++)
+    if (a == b)
     {
-        var key = arr[i];
-        var flag = 0;
-        for (int j = i - 1; j >= 0 && flag != 1;)
+        return true;
+    }
+
+    return false;
+}
+
+// Analyze that will return number of alphabetic chars in string,
+// digits and another special characters (void Analyze(string))
+
+void Analyze(string text)
+{
+    int digits=0, letters=0, symbols=0;
+    
+    foreach (char x in text)
+    {
+        if (char.IsLetter(x))
         {
-            if (key < arr[j])
+            letters++;
+    
+        }
+        else if (char.IsDigit(x))
+        {
+            digits++;
+        }
+        else 
+        {
+            symbols++;
+        }
+    
+    }
+    Console.WriteLine(letters);
+    Console.WriteLine(digits);
+    Console.WriteLine(symbols);
+}
+
+// Sort that will return string that contains all characters from input string
+// sorted in alphabetical order (e.g. 'Hello' -> 'ehllo') (string Sort(string))
+string Sort(string text)
+{
+    char[] chars = new char[text.Length];
+    
+    string low = text.ToLower(CultureInfo.CurrentCulture);
+    
+    
+    for (int i = 0; i < text.Length; i++)
+    {
+        chars[i] = low[i];
+    }
+
+    Array.Sort(chars);
+    return new string(chars);
+}
+
+// Duplicate that will return array of characters that are
+// duplicated in input string (e.g. 'Hello and hi' -> ['h', 'l']) (char[] Duplicate(string))
+char[] Duplicate(string str)
+{
+    string lower = str.ToLower(CultureInfo.CurrentCulture);
+    char[] duplicat = Array.Empty<char>();
+
+    for (int i = 0; i < lower.Length - 1; i++)
+    {
+        if (str[(i + 1)..].Contains(lower[i]))
+        {
+            bool alreadyInsideDuplicates = false;
+            for (int j = 0; j < duplicat.Length; j++)
             {
-                arr[j + 1] = arr[j];
-                j--;
-                arr[j + 1] = key;
+                if (duplicat[j] == lower[i])
+                {
+                    alreadyInsideDuplicates = true;
+                    break;
+                }
             }
-            else flag = 1;
+
+            if (!alreadyInsideDuplicates)
+            {
+                Array.Resize(ref duplicat, duplicat.Length + 1);
+                duplicat[^1] = lower[i];
+            }
         }
     }
-    return arr;
+
+    return duplicat;
 }
+Console.WriteLine("Compare Method");
+Console.WriteLine(Compare("sdfsd", "dsfsd"));
+Console.WriteLine(Compare("sdfsd", "dsfsd", false));
+Console.WriteLine(Compare("sdfsd", "sdfsd"));
+Console.WriteLine(Compare("sdfsd", "Sdfsd", false));
+
+Console.WriteLine("Analyze Method");
+Analyze("aasdas IJIUIUH £%$%^");
+Console.WriteLine("Sort Method");
+Console.WriteLine(Sort("aasdas IJIUIUH fsejdfj"));
+Console.WriteLine("Duplicate Method");
+Console.WriteLine(Duplicate("aasdas IJIUIUH fsejdfj"));
