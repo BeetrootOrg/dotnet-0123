2d1c26101123a5e4c85a5b8530e72ccac28d0349
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-20 19:24:06 +0200

    binding flags

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,8 +13,44 @@ foreach (PropertyInfo propertyInfo in testType.GetProperties())
     Console.WriteLine($"Type {testType.Name} has property {propertyInfo.Name} with type {propertyInfo.PropertyType.Name}");
 }
 
+foreach (PropertyInfo propertyInfo in testType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+{
+    Console.WriteLine($"Type {testType.Name} has non-public property {propertyInfo.Name} with type {propertyInfo.PropertyType.Name}");
+}
+
 internal class Test
 {
-    public int Number { get; set; }
-    public string Value { get; set; }
+    private static readonly int PrivateStaticFieldNumber;
+    public static int PublicStaticFieldNumber;
+
+    private static int PrivateStaticNumber { get; set; }
+    public static int PublicStaticNumber { get; set; }
+
+    private readonly int _privateFieldNumber;
+    public int PublicFieldNumber;
+
+    private int PrivateNumber { get; set; }
+
+    public int PublicNumber { get; set; }
+    public string PublicValue { get; set; }
+
+    public static int GetPrivateStaticFieldNumber()
+    {
+        return PrivateStaticFieldNumber;
+    }
+
+    public int GetPrivateFieldNumber()
+    {
+        return _privateFieldNumber;
+    }
+
+    public static int GetPrivateStaticNumber()
+    {
+        return PrivateStaticNumber;
+    }
+
+    public int GetPrivateNumber()
+    {
+        return PrivateNumber;
+    }
 }
