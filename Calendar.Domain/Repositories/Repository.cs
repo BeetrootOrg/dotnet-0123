8dad9eddd8e3c8ebf5f9e54c3da82dfa2d747013
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-24 20:16:38 +0200

    use json

--- Calendar.Domain/Repositories/Repository.cs
+++ Calendar.Domain/Repositories/Repository.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Text.Json;
 
 using Calendar.Contracts;
 
@@ -31,37 +30,19 @@ namespace Calendar.Domain.Repositories
 
         private void DumpToFile()
         {
-            File.WriteAllLines(
-                _filename,
-                _meetings.Select(meeting => $"{meeting.Name},{meeting.Start},{meeting.Duration},{meeting.Room.Name}")
-                    .Prepend("Name,Start,Duration,Room")
-            );
+            using FileStream file = File.Create(_filename);
+            JsonSerializer.Serialize(file, _meetings);
         }
 
         public static IRepository CreateRepository(string filename)
         {
-            return !File.Exists(filename)
-                ? new Repository(filename, new List<Meeting>())
-                : new Repository(
-                filename,
-                File.ReadAllLines(filename)
-                    .Skip(1)
-                    .Select(line =>
-                    {
-                        string[] items = line.Split(',');
-                        return new Meeting
-                        {
-                            Name = items[0],
-                            Start = DateTime.Parse(items[1]),
-                            Duration = TimeSpan.Parse(items[2]),
-                            Room = new Room
-                            {
-                                Name = items[3]
-                            }
-                        };
-                    })
-                    .ToList()
-            );
+            if (!File.Exists(filename))
+            {
+                return new Repository(filename, new List<Meeting>());
+            }
+
+            using FileStream file = File.OpenRead(filename);
+            return new Repository(filename, JsonSerializer.Deserialize<IList<Meeting>>(file));
         }
     }
 }
