a0054299fa8d0e5987e30233a3c5f0ec48985eb7
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-24 19:23:39 +0200

    listen to key

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ConsoleKeyInfo? key1 = null;
+
+// 1st option
+Timer timer = new((state) => Console.WriteLine(key1?.Key), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+// 2nd option
+Task.Run(async () =>
+{
+    while (true)
+    {
+        Console.WriteLine(key1?.Key);
+        await Task.Delay(1000);
+    }
+});
+
+while (true)
+{
+    key1 = Console.ReadKey(false);
+}
