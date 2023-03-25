63de056a3230ee20ce1c6d4ba06bf2a0b5070967
Artem <shumckiy2004@gmail.com>
2023-03-25 21:05:30 +0200

    17 homework

---
+++ Vote.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp
+{
+    public class Vote
+    {
+        public string voteName {get; set;}
+        public int optionsCount {get; set;}
+        public string[] options {get; set;}
+        public int[] votes {get; set;}
+    }
+}
