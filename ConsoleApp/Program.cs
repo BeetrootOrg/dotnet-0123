8037e6b947f6554504ece4daac2e2492aa7ee443
Alexander Strekanov <strekanov.alexander@gmail.com>
2023-03-25 12:33:27 +0200

    homework-21

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+var assembly = typeof(Console).Assembly;
+Console.WriteLine($"Assembly location: {assembly.Location}");
+
+List<Type> list = new List<Type>(); 
+
+foreach(Type item in assembly.GetTypes())
+{
+    list.Add(item);    
+}
+
+Type type = list.Where(p => p.FullName == "System.Console").First();
+
+Console.WriteLine($"ConsoleColor properties of type: {type.Name}");
+foreach (PropertyInfo prop in type.GetProperties(BindingFlags.Public | BindingFlags.Static))
+{
+    if (prop.PropertyType == typeof(ConsoleColor))
+    {
+        Console.WriteLine($"\t{prop.Name}. Type: {prop.PropertyType.Name}");
+        if (prop.Name == "ForegroundColor")
+        {
+            var defaultForegroundColor = prop.GetValue(prop); 
+            Console.WriteLine(prop.GetValue(prop));
+            prop.SetValue(prop, ConsoleColor.Blue);
+            Console.WriteLine("Changed foreground color is on this string");
+            prop.SetValue(prop, defaultForegroundColor);
+        }
+    }    
+}
+
+Console.WriteLine($"Example method from type: {type.Name}");
+foreach (MethodInfo method in type.GetMethods())
+{
+    if (method.ToString() == "System.String get_Title()")
+    {
+        object? consoleTitle = method.Invoke(method, Array.Empty<object>());
+        if (consoleTitle != null)
+        {
+            Console.WriteLine(consoleTitle);
+        }
+    }    
+}
