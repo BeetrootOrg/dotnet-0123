f26dd7c97ed836b57b2057e3f653bebfe89592f9
igorbelovash <belovash2002@gmail.com>
2023-03-26 13:40:36 +0300

    classwork

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,87 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 
 using ConsoleApp;
 
-AddOverflowDelegate overflow = (long result) => { return (int)result;}; //AddOverflowDelegate overflow = (long result) => (int)result;
-AddSuccessDelegate success = (int result) => Console.WriteLine(result);
+int[] numbers = new[] {1, 2, 3, 4, 5};
+string[] words = new[] { "one", "two", "three", "four", "five" };
 
-int result1 = Test.Add(1, 2, overflow, success);
-int result2 = Test.Add(int.MaxValue, 1, overflow, success);
-
-int result3 = Test.Add(10, 20, 
-    (long result) => throw new Exception("Unexpected"), 
-    (int result) => Console.WriteLine($"10 + 20 ={result}"));
-
-List<int> numbers = new List<int>();
-
-OnChangeableChange writeLine = (obj, args) =>
-
-        Console.WriteLine($"Number changed from {args.Old} to {args.New}");
-
-OnChangeableChange tracker = (obj, args) =>
-
-        numbers.Add(args.Old);
-
-
-Changeable c1 = new Changeable
-{
-    Number = 42
-};
-
-c1.OnChangeableChange += writeLine;
-c1.OnChangeableChange += tracker;
-
-c1.Number = 43;
-c1.Number = 44;
-c1.Number = 44;
-c1.Number = 42;
-
-Console.WriteLine(string.Join(", ", numbers));
-
-foreach (int item in new WhereEnumerable<int>(new[] { 1, 2, 3, 4 }, (item) => item % 2 == 0))
+//ANALOGUE
+//foreach (int item in new WhereEnumerable<int>(new[] { 1, 2, 3, 4 }, (item) => item % 2 == 0))
+foreach (var item in numbers.Where((item) => item % 2 == 0))
 {
     Console.WriteLine(item);
 }
 
-foreach (string item in new WhereEnumerable<string>(new[] { "hello", "world", "!" }, (item) => item.Length > 3))
+foreach (int item in numbers.Select((item) => item * 2))
 {
     Console.WriteLine(item);
 }
 
-#pragma warning disable
-foreach (string item in new WhereEnumerable<object>(new object[] { "hello", 1, "world", 2, "!", 3 }, (item) => item is string))
-#pragma warning restore
+foreach (string item in numbers.Zip(words, (number, word) => $"{number} - {word}"))
 {
     Console.WriteLine(item);
 }
 
-int called = 0;
-Timer t1 = new((state) => Console.WriteLine($"Called: {++called}"),
-    null,
-    TimeSpan.Zero, //0
-    TimeSpan.FromSeconds(1));
-
-Timer t2 = new((state) => Console.WriteLine("Second timer"),
-    null,
-    Timeout.Infinite,
-    Timeout.Infinite);
-
-Thread.Sleep(5000);
-Console.ForegroundColor = ConsoleColor.Yellow;
-t2.Change(TimeSpan.Zero, TimeSpan.FromSeconds(1));
-Thread.Sleep(5000);
-
-int[] number = new[] { 1, 2, 3, 4, 5 };
-string[] words = new[] { "one", "two", "three", "four" };
-
-foreach (string item in new ZipEnumerable<int, string, string>(numbers, words, (num, word) => $"Number '{num}' pronounces as '{word}'"))
+// ANALOGUE BELOW
+// foreach (var item in new SelectEnumerable<int, int>(new WhereEnumerable<int>(numbers, (item) => item % 2 == 0), (item) => item * 2))
+foreach (int item in numbers.Where((item) => item % 2 == 0).Select((item) => item * 2))
 {
     Console.WriteLine(item);
 }
 
-foreach (int item in new SelectEnumerable<int, int>(numbers, (num) => num * num))
+foreach (IEnumerable<int> item in new[] { 1, 2, 3, 4 }.ChunkBy(2))
 {
-    Console.WriteLine(item);
+    Console.WriteLine(string.Join(", ", item));
 }
+
+foreach (IEnumerable<int> item in new[] { 1, 2, 3 }.ChunkBy(2))
+{
+    Console.WriteLine(string.Join(", ", item));
+}
+
+foreach (IEnumerable<int> item in new[] { 1, 2, 3 }.ChunkBy(5))
+{
+    Console.WriteLine(string.Join(", ", item));
+}
+
+IEnumerable<int>[] result = System.Linq.Enumerable.ToArray(new[] { 1, 2, 3, 4 }.ChunkBy(2));
+foreach (IEnumerable<int> item in result)
+{
+    Console.WriteLine(string.Join(", ", item));
+}
+
+Console.WriteLine(string.Join(", ", new DateOnly(2023, 3, 1).DaysBetween(new DateOnly(2023, 3, 15))));
+Console.WriteLine(string.Join(", ", new DateOnly(2023, 3, 1).DaysBetween(new DateOnly(2023, 3, 1))));
+
+Console.WriteLine(new DateOnly(1996, 07, 11).Age());
+Console.WriteLine(new DateOnly(1996, 03, 15).Age());
+Console.WriteLine(new DateOnly(1996, 03, 16).Age());
+
+Console.WriteLine(new DateOnly(2023, 03, 15).IsWeekend());
+Console.WriteLine(new DateOnly(2023, 03, 17).IsWeekend());
+Console.WriteLine(new DateOnly(2023, 03, 18).IsWeekend());
+Console.WriteLine(new DateOnly(2023, 03, 19).IsWeekend());
+Console.WriteLine(new DateOnly(2023, 03, 20).IsWeekend());
+
+Console.WriteLine(new DateOnly(2023, 03, 15).NextWorkday());
+Console.WriteLine(new DateOnly(2023, 03, 17).NextWorkday());
+Console.WriteLine(new DateOnly(2023, 03, 18).NextWorkday());
+Console.WriteLine(new DateOnly(2023, 03, 19).NextWorkday());
+Console.WriteLine(new DateOnly(2023, 03, 20).NextWorkday());
+
+Console.WriteLine("TrUE".ToBool());
+Console.WriteLine("FALse".ToBool());
+Console.WriteLine("0".ToBool());
