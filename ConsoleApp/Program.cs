3a0b7a9244cdbdf468e486c737ff0a6054d7f934
excellent <flarpzz08@gmail.com>
2023-03-26 14:48:57 +0200

    initial commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp;
+
+ConsoleApp.Stack<int> s = new();
+int[] arr = new int[2];
+s.Push(1);
+s.Push(3);
+s.Push(8);
+
+s.CopyTo(arr);
+s.Clear();
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine(s.Count);
