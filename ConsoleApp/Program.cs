09f908914d272b0e1f770f679a1362972b9cfaa4
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-27 19:50:31 +0300

    WhenAll

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,57 +5,18 @@ using System.Threading.Tasks;
 Stopwatch sw = new();
 
 sw.Start();
-Task boilAKettleTask = BoilAKettle();
-Task heatUpAPanTask = HeatUpAPan();
-Task pourAGlassOfWater = PourAGlassOfWater();
-await AddButterToBread();
-await heatUpAPanTask;
-await FryTwoEggs();
-await Task.WhenAll(boilAKettleTask, pourAGlassOfWater);
-await PrepareCoffee();
-await Breakfast();
+
+Task t1 = DelayAMessage(TimeSpan.FromSeconds(1), "First message");
+Task t2 = DelayAMessage(TimeSpan.FromSeconds(2), "Second message");
+Task t3 = DelayAMessage(TimeSpan.FromSeconds(3), "Third message");
+
+await Task.WhenAll(t1, t2, t3);
 sw.Stop();
 
-Console.WriteLine($"Breakfast took {sw.ElapsedMilliseconds}");
+Console.WriteLine($"Finished in {sw.ElapsedMilliseconds}");
 
-async Task BoilAKettle()
+static async Task DelayAMessage(TimeSpan delay, string message)
 {
-    await Task.Delay(5000);
-    Console.WriteLine("Kettle is boiled!");
-}
-
-async Task HeatUpAPan()
-{
-    await Task.Delay(2000);
-    Console.WriteLine("Pan ready!");
-}
-
-async Task FryTwoEggs()
-{
-    await Task.Delay(7000);
-    Console.WriteLine("Eggs ready!");
-}
-
-async Task AddButterToBread()
-{
-    await Task.Delay(1500);
-    Console.WriteLine("Bread ready!");
-}
-
-async Task PrepareCoffee()
-{
-    await Task.Delay(2000);
-    Console.WriteLine("Coffee ready!");
-}
-
-async Task PourAGlassOfWater()
-{
-    await Task.Delay(1000);
-    Console.WriteLine("Water in glass!");
-}
-
-async Task Breakfast()
-{
-    await Task.Delay(10000);
-    Console.WriteLine("Breakfast eaten!");
+    await Task.Delay(delay);
+    Console.WriteLine(message);
 }
