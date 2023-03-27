4b6d6999ab044942dfb25e5d470e90940b2a292c
Dmytro Misik <misik.dmitriy@gmail.com>
2023-03-27 19:58:14 +0300

    WhenAny

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-
-Stopwatch sw = new();
-
-sw.Start();
 
 Task t1 = DelayAMessage(TimeSpan.FromSeconds(1), "First message");
 Task t2 = DelayAMessage(TimeSpan.FromSeconds(2), "Second message");
 Task t3 = DelayAMessage(TimeSpan.FromSeconds(3), "Third message");
+List<Task> tasks = new() { t1, t2, t3 };
 
-await Task.WhenAll(t1, t2, t3);
-sw.Stop();
-
-Console.WriteLine($"Finished in {sw.ElapsedMilliseconds}");
+while (tasks.Any())
+{
+    Task result = await Task.WhenAny(tasks);
+    Console.WriteLine("One of tasks finished");
+    _ = tasks.Remove(result);
+}
 
 static async Task DelayAMessage(TimeSpan delay, string message)
 {
