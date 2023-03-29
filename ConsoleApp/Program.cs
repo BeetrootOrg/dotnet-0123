a6a057899a93e315650fe996a7a609888cf7d8da
igorbelovash <belovash2002@gmail.com>
2023-03-29 12:57:09 +0300

    classwork

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,75 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
 using ConsoleApp;
 
-int[] numbers = new[] {1, 2, 3, 4, 5};
-string[] words = new[] { "one", "two", "three", "four", "five" };
 
-//ANALOGUE
-//foreach (int item in new WhereEnumerable<int>(new[] { 1, 2, 3, 4 }, (item) => item % 2 == 0))
-foreach (var item in numbers.Where((item) => item % 2 == 0))
+
+IEnumerable<Person> persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+
+Console.WriteLine($"People count = {persons.Count()}");
+
+Func<Person, bool> malePredicate = (Person p) => p.Gender is Gender.Male;
+
+Console.WriteLine($"Male count = {persons.Count(malePredicate)}");
+Console.WriteLine($"Female count = {persons.Count(p => p.Gender is Gender.Female)}");
+
+int youngestAge = persons.Min(p => p.Age);
+Person youngest = persons.MinBy(p => p.Age);
+
+Console.WriteLine($"Youngest age = {youngestAge}");
+Console.WriteLine($"Youngest person = {youngest}");
+Console.WriteLine($"Youngest person name = {youngest.Name}");
+
+int oldestAge = persons.Max(p => p.Age);
+Person oldest = persons.MaxBy(p => p.Age);
+
+Console.WriteLine($"Oldest age = {oldestAge}");
+Console.WriteLine($"Oldest person = {oldest}");
+Console.WriteLine($"Oldest person name = {oldest.Name}");
+
+double averageAge = persons.Average(p => p.Age);
+Console.WriteLine($"Average age = {averageAge}");
+
+foreach (IGrouping<string, Person> group in persons.GroupBy(p => p.EyeColor))
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"There are {group.Count()} with {group.Key} eye color");
+}
+//foreach (var group in persons.GroupBy(p => p.Gender))
+//{
+//  Console.WriteLine($"There are {group.Count()} {group.Key.ToString().ToLower(CultureInfo.CurrentCulture)}s");
+//}
+foreach (IGrouping<Gender, Person> group in persons.GroupBy(p => p.Gender))
+{
+    string gender = group.Key.ToString().ToLower(CultureInfo.CurrentCulture);
+    Console.WriteLine($"There are {group.Count()} {gender}s");
 }
 
-foreach (int item in numbers.Select((item) => item * 2))
+// var names = person.Select
+IEnumerable<Name> names = persons.Select(p =>
+    {
+        string[] splitted = p.Name.Split(' ');
+        return new Name
+        {
+            FirstName = splitted[0],
+            LastName = splitted[1]
+        };
+    })
+    .ToArray();  
+
+Console.WriteLine(string.Join(Environment.NewLine, names));
+
+IEnumerable<IGrouping<string, string>> nonUniqueNames = names.Select(n => n.FirstName)
+    .GroupBy(x => x)
+    .Where(group => group.Count() > 1);
+
+// foreach (var group in nonUniqueNames)
+foreach (IGrouping<string, string> group in nonUniqueNames)
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"Name {group.Key} appeared {group.Count()} times");
 }
 
-foreach (string item in numbers.Zip(words, (number, word) => $"{number} - {word}"))
+foreach (var group in persons.Join(
+                                    persons,
+                                    p => true,
+                                    p => true,
+                                    (p1, p2) => new
+                                    {
+                                        First = p1,
+                                        Second = p2
+                                    }
+                                ).Where(group => !ReferenceEquals(group.First, group.Second))
+                                .Where(group => group.First.Company == group.Second.Company))
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"Potential coleagues are {group.First.Name} and {group.Second.Name}");
 }
 
-// ANALOGUE BELOW
-// foreach (var item in new SelectEnumerable<int, int>(new WhereEnumerable<int>(numbers, (item) => item % 2 == 0), (item) => item * 2))
-foreach (int item in numbers.Where((item) => item % 2 == 0).Select((item) => item * 2))
-{
-    Console.WriteLine(item);
-}
+Person theFriendestPerson = persons.MaxBy(p => p.Friends.Length);
+Console.WriteLine($"Person with max friends number is {theFriendestPerson.Name} with {theFriendestPerson.Friends.Length} friends");
 
-foreach (IEnumerable<int> item in new[] { 1, 2, 3, 4 }.ChunkBy(2))
-{
-    Console.WriteLine(string.Join(", ", item));
-}
+// 1st approach
 
-foreach (IEnumerable<int> item in new[] { 1, 2, 3 }.ChunkBy(2))
-{
-    Console.WriteLine(string.Join(", ", item));
-}
+//var friendestPeople1 = persons.GroupBy(p => p.Friends.Length)
+IGrouping<int, Person> friendestPeople1 = persons.GroupBy(p => p.Friends.Length)
+    .MaxBy(group => group.Key);
 
-foreach (IEnumerable<int> item in new[] { 1, 2, 3 }.ChunkBy(5))
-{
-    Console.WriteLine(string.Join(", ", item));
-}
+Console.WriteLine($"People with max friends number are {string.Join(", ", friendestPeople1.Select(p => p.Name))} with {friendestPeople1.Key} friends each");
 
-IEnumerable<int>[] result = System.Linq.Enumerable.ToArray(new[] { 1, 2, 3, 4 }.ChunkBy(2));
-foreach (IEnumerable<int> item in result)
-{
-    Console.WriteLine(string.Join(", ", item));
-}
+// 2nd approach
+int maxFriendsNumber = persons.Max(p => p.Friends.Length);
+Person[] friendestPeople2 = persons.Where(p => p.Friends.Length == maxFriendsNumber).ToArray();
 
-Console.WriteLine(string.Join(", ", new DateOnly(2023, 3, 1).DaysBetween(new DateOnly(2023, 3, 15))));
-Console.WriteLine(string.Join(", ", new DateOnly(2023, 3, 1).DaysBetween(new DateOnly(2023, 3, 1))));
+Console.WriteLine($"There are {friendestPeople2.Length} people with {maxFriendsNumber} of friends");
 
-Console.WriteLine(new DateOnly(1996, 07, 11).Age());
-Console.WriteLine(new DateOnly(1996, 03, 15).Age());
-Console.WriteLine(new DateOnly(1996, 03, 16).Age());
+Console.WriteLine($"Everybody registered in past = {persons.All(p => p.Registered < DateTime.Now)}");
+Console.WriteLine($"Anybody without friend = {persons.Any(p => p.Friends.Length == 0)}");
 
-Console.WriteLine(new DateOnly(2023, 03, 15).IsWeekend());
-Console.WriteLine(new DateOnly(2023, 03, 17).IsWeekend());
-Console.WriteLine(new DateOnly(2023, 03, 18).IsWeekend());
-Console.WriteLine(new DateOnly(2023, 03, 19).IsWeekend());
-Console.WriteLine(new DateOnly(2023, 03, 20).IsWeekend());
+IEnumerable<Person> distinctByEyeColor = persons.DistinctBy(p => p.EyeColor);
+Console.WriteLine($"{nameof(distinctByEyeColor)}.Count() = {distinctByEyeColor.Count()}");
 
-Console.WriteLine(new DateOnly(2023, 03, 15).NextWorkday());
-Console.WriteLine(new DateOnly(2023, 03, 17).NextWorkday());
-Console.WriteLine(new DateOnly(2023, 03, 18).NextWorkday());
-Console.WriteLine(new DateOnly(2023, 03, 19).NextWorkday());
-Console.WriteLine(new DateOnly(2023, 03, 20).NextWorkday());
+Person firstPerson = persons.First();
+Person firstWith4Friends = persons.First(p => p.Friends.Length == 4);
 
-Console.WriteLine("TrUE".ToBool());
-Console.WriteLine("FALse".ToBool());
-Console.WriteLine("0".ToBool());
+Console.WriteLine($"First = {firstPerson}");
+Console.WriteLine($"First with 4 friend = {firstWith4Friends}");
+
+Person firstWith6Friends = persons.FirstOrDefault(p => p.Friends.Length == 6);
+Console.WriteLine($"First with 6 friend = {firstWith6Friends}");
+
+Person personWithExactName = persons.Single(p => p.Name == "Holloway Meyers");
+Console.WriteLine($"Person with exact name = {personWithExactName}");
+
+Person orderedPerson = persons.OrderBy(x => x.Name).First();
+Console.WriteLine($"First person = {orderedPerson}");
+
+IEnumerable<Friend[]> allFriendsBad = persons.Select(x => x.Friends);
+IEnumerable<Friend> allFriendsGood = persons.SelectMany(x => x.Friends);
