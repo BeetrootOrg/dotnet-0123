05475f16898e3a04d2c55e97360b8b671038b505
Dmytro Misik <misik.dmitriy@gmail.com>
2023-04-03 19:54:52 +0300

    insert and select

---
+++ 02.create.sql
@@ -0,0 +1,9 @@
+INSERT INTO tbl_persons(first_name, last_name, age, gender)
+VALUES('Dima', 'Misik', 26, 'M'),
+    ('Stan', 'Smith', 32, 'M'),
+    ('John', 'Doe', 45, 'M'),
+    ('Jane', 'Doe', 42, 'F'),
+    ('Mary', 'Smith', 38, 'F'),
+    ('Sara', 'Smith', 18, 'F'),
+    ('Sara', 'Doe', 18, 'F'),
+    ('John', 'Smith', 18, 'M');
