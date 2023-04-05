7e6d57ca0f2957214b8745383e4b7a811fe7c66c
Dmytro Misik <misik.dmitriy@gmail.com>
2023-04-05 20:54:49 +0300

    shop

--- 02.shop.sql
+++ 02.shop.sql
@@ -6,3 +6,23 @@ CREATE TABLE IF NOT EXISTS tbl_shop(
     createdAt TIMESTAMP NOT NULL DEFAULT NOW(),
     quantity INT NOT NULL CHECK(quantity >= 0)
 );
+-- add column unit
+ALTER TABLE tbl_shop
+ADD COLUMN unit SMALLINT NOT NULL DEFAULT 0 CHECK (
+        unit >= 0
+        AND unit <= 1
+    );
+-- change column quantity type
+ALTER TABLE tbl_shop
+ALTER COLUMN quantity TYPE NUMERIC(7, 3);
+-- delete quantity check contraint
+ALTER TABLE tbl_shop DROP CONSTRAINT tbl_shop_quantity_check;
+-- add quantity check contraint
+ALTER TABLE tbl_shop
+ADD CONSTRAINT tbl_shop_quantity_check CHECK (
+        quantity >= 0
+        AND (
+            unit <> 0
+            OR quantity % 1 = 0
+        )
+    );
