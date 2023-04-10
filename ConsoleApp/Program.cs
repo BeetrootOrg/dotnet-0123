874bbaa273743fd7b120b55e42de4afa2de5944e
Dmytro Misik <misik.dmitriy@gmail.com>
2023-04-10 19:44:54 +0300

    join

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,8 +8,15 @@ await using ShopContext context = new();
 int customersCount = await context.TblCustomers.CountAsync();
 Console.WriteLine($"There are {customersCount} customers");
 
-TblCustomer[] customers = await context.TblCustomers.ToArrayAsync();
+TblCustomer[] customers = await context.TblCustomers
+    .Include(customer => customer.TblOrders)
+    .ThenInclude(order => order.Employee)
+    .Include(customer => customer.TblOrders)
+    .ThenInclude(order => order.TblOrdersProducts)
+    .ThenInclude(orderProduct => orderProduct.Product)
+    .ToArrayAsync();
+
 foreach (TblCustomer customer in customers)
 {
-    Console.WriteLine($"Customer #{customer.Id} ({customer.FirstName} {customer.LastName})");
+    Console.WriteLine($"Customer #{customer.Id} ({customer.FirstName} {customer.LastName}) has {customer.TblOrders.Count} orders");
 }
