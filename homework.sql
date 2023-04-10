6c8486a041b17b7353df00812f34a7f644d9e36d
maksymolinyk <maxim.oliynyk75@gmail.com>
2023-04-10 15:33:38 +0300

    library db

--- homework.sql
+++ homework.sql
@@ -1,50 +1,46 @@
--- 01. Create table for ‘phone book’
-CREATE TABLE IF NOT EXISTS tbl_phone_book (
-    id BIGSERIAL NOT NULL ,
+-- 01.1 create table to store customer information
+CREATE TABLE IF NOT EXISTS tbl_customers (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    phone VARCHAR(12) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+    phone VARCHAR(12) NOT NULL,
+    address VARCHAR(255) NOT NULL
 );
 
--- 02. Create table to store school schedule
-CREATE TABLE IF NOT EXISTS tbl_school_schedule (
-    id BIGSERIAL NOT NULL,
-    class_name VARCHAR(50) NOT NULL,
-    class_time TIMESTAMP NOT NULL,
-    class_room VARCHAR(50) NOT NULL,
-    teacher_name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+-- 01.2 create table to store author information
+CREATE TABLE IF NOT EXISTS tbl_authors (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birth_year INT NOT NULL,
+    death_year INT
 );
 
---03. Create table to store user’s login history
-CREATE TABLE IF NOT EXISTS tbl_login_history (
-    id BIGSERIAL NOT NULL,
-    user_nickname VARCHAR(50) NOT NULL,
-    user_email VARCHAR(50) NOT NULL,
-    login_time TIMESTAMP NOT NULL,
-    logout_time TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
+-- 01.3 create table to store book information
+CREATE TABLE IF NOT EXISTS tbl_books (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    isbn VARCHAR(13) NOT NULL,
+    author_id BIGINT NOT NULL,
+	amount INT NOT NULL,
+	year INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES tbl_authors(id)
 );
 
--- 04. Create table to store bank accounts
-CREATE TABLE IF NOT EXISTS tbl_bank_accounts (
-    id BIGSERIAL NOT NULL,
-    account_number BIGINT NOT NULL,
-    account_owner_first_name VARCHAR(50) NOT NULL,
-    account_owner_last_name VARCHAR(50) NOT NULL,
-    account_balance MONEY NOT NULL,
-    PRIMARY KEY (id)
+-- 01.4 create table to store order information
+CREATE TABLE IF NOT EXISTS tbl_orders (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    customer_id BIGINT NOT NULL,
+    book_id BIGINT NOT NULL,
+    taken_date TIMESTAMP NOT NULL,
+    return_date TIMESTAMP NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES tbl_customers(id),
+    FOREIGN KEY (book_id) REFERENCES tbl_books(id)
 );
 
--- 05. Create table to store bank transactions data
-CREATE TABLE IF NOT EXISTS tbl_bank_transactions (
-    id BIGSERIAL NOT NULL,
-    account_number BIGINT NOT NULL,
-    transaction_date TIMESTAMP NOT NULL,
-    transaction_type VARCHAR(50) NOT NULL,
-    transaction_amount MONEY NOT NULL,
-    transaction_description VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- 01.5 alter tbl_orders to return_date to be nullable
+ALTER TABLE tbl_orders
+ALTER COLUMN return_date DROP NOT NULL;
+
+
