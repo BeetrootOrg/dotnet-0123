8dbe8959d9b75b5a439035bfff14037cc1b7fd9e
excellent <flarpzz08@gmail.com>
2023-04-13 00:36:44 +0200

    GroupBy homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using ConsoleApp;
+
+Person[] people =
+{
+      new Person("Aaron", "Bank of India"), // 3
+      new Person("Finn", "Amazon"), // 1
+      new Person("Bernard", "Canara Bank"), // 5 
+      new Person("Asher", "Canara Bank"), // 6
+      new Person("Kate", "Bank of India"), // 4
+      new Person("Alice", "Amazon"), // 2
+  };
+
+var companies = people.GroupByEnumerable(p => p.Company);
+foreach (var company in companies)
+{
+    Console.WriteLine(company.Key);
+    foreach (var person in company)
+    {
+        Console.WriteLine(person.Name);
+    }
+    Console.WriteLine();
+}
+
+record class Person(string Name, string Company);
+
+
+
