b91ec959307a974e22c70af3ae26de4c4c744ddd
igorbelovash <belovash2002@gmail.com>
2023-04-12 20:03:43 +0300

    classwork

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 var t1 = DelayAMessage(TimeSpan.FromSeconds(1), "First message");
 var t2 = DelayAMessage(TimeSpan.FromSeconds(2), "Second message");
 var t3 = DelayAMessage(TimeSpan.FromSeconds(3), "Third message");
+var tasks = new List<Task> {t1, t2, t3};
 
-Stopwatch sw = new();
+// Stopwatch sw = new();
 
-sw.Start();
-await Task.WhenAll(t1, t2, t3);
-sw.Stop();
-
-Console.WriteLine($"Finished in {sw.ElapsedMilliseconds}");
+// sw.Start();
+while (tasks.Any())
+{
+// await Task.WhenAll(t1, t2, t3);
+    var result = await Task.WhenAny(tasks);
+    Console.WriteLine("One task finished ");
+    tasks.Remove(result);
+}
+// Console.WriteLine($"Finished in {sw.ElapsedMilliseconds}");
 
 static async Task DelayAMessage(TimeSpan delay, string message)
 {
