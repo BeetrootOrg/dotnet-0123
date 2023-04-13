e8853689f1a7493febe73fc52a21811b00048e4b
IrynaDonienko <114020848+IrynaDonienko@users.noreply.github.com>
2023-04-13 23:59:03 +0200

    change

---
+++ ConsoleApp/Models/TblAuthor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.Models;
+
+public partial class TblAuthor
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public virtual ICollection<TblBook> TblBooks { get; set; } = new List<TblBook>();
+}
---
+++ ConsoleApp/Models/TblCounting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.Models;
+
+public partial class TblCounting
+{
+    public int Id { get; set; }
+
+    public int BookId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public DateTime IssueDate { get; set; }
+
+    public DateTime ReturnDate { get; set; }
+
+    public virtual TblBook Book { get; set; } = null!;
+
+    public virtual TblCustomer Customer { get; set; } = null!;
+}
