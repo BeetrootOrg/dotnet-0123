be6726e609775985aa74fa6642218e06d3cb8b8c
Andrey Tzarkov <admyndc@gmail.com>
2023-04-17 02:11:33 +0300

    prepare

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// -- Homework 30-Entity Framework
+// -- CodeFirst
+// --- declare SQL and C# models for the ‘book library’ domain, including:
+// --    books
+// --    authors
+// --    count of each book
+// --    customers
+// --    history which book was taken by whom and when
