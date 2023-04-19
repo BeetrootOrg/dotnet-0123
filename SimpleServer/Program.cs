12f075f3efdd3ae13c046acf45864fb0d4cb11d0
Dmytro Misik <misik.dmitriy@gmail.com>
2023-04-19 20:48:23 +0300

    fix API documentation

--- SimpleServer/Program.cs
+++ SimpleServer/Program.cs
@@ -1,7 +1,12 @@
+using System;
+using System.IO;
+using System.Reflection;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 using SimpleServer.Context;
 
@@ -10,7 +15,18 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PersonDbContext>(options => options.UseInMemoryDatabase("PersonDbContext"));
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Persons API",
+        Description = "An ASP.NET Core Web API for managing persons",
+    });
+
+    string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 WebApplication app = builder.Build();
 
