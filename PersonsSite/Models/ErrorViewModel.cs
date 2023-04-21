2cdaf8926289086c026df4614b83273bb894a40d
Dmytro Misik <misik.dmitriy@gmail.com>
2023-04-21 19:30:04 +0300

    asp.net initial

---
+++ PersonsSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonsSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
