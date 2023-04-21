41076b7d4c38803278d5f57eae7cf0811d772bb8
Dmytro Misik <misik.dmitriy@gmail.com>
2023-04-21 20:32:15 +0300

    delete

--- PersonsSite/Controllers/PersonsController.cs
+++ PersonsSite/Controllers/PersonsController.cs
@@ -40,5 +40,22 @@ namespace PersonsSite.Controllers
 
             return View(person);
         }
+
+        public async Task<IActionResult> Delete([FromRoute] int id, CancellationToken cancellationToken = default)
+        {
+            Person person = await _context.Persons.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+            return person == null ? NotFound() : View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePerson([FromRoute] int id, CancellationToken cancellationToken = default)
+        {
+            _ = _context.Persons.Remove(new Person { Id = id });
+            _ = await _context.SaveChangesAsync(cancellationToken);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
