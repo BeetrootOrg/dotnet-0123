93f981d95942c04f55396a1c9b313ab88bfb464d
Artem <shumckiy2004@gmail.com>
2023-04-23 23:47:59 +0300

    29 homework

---
+++ Models/TblAuthor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnet_0123.Models;
+
+public partial class TblAuthor
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public virtual ICollection<TblBook> TblBooks { get; set; } = new List<TblBook>();
+}
---
+++ Models/TblBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnet_0123.Models;
+
+public partial class TblBook
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public long AuthorId { get; set; }
+
+    public string Genre { get; set; } = null!;
+
+    public DateTime Year { get; set; }
+
+    public virtual TblAuthor Author { get; set; } = null!;
+
+    public virtual ICollection<TblOrder> TblOrders { get; set; } = new List<TblOrder>();
+}
