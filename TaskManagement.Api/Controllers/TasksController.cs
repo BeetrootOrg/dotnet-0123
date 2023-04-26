9c098c3605b19da5487cdc05258a4afee39710dd
Dmytro Misik <misik.dmitriy@gmail.com>
2023-04-26 20:29:07 +0300

    swagger documentation

--- TaskManagement.Api/Controllers/TasksController.cs
+++ TaskManagement.Api/Controllers/TasksController.cs
@@ -13,6 +13,7 @@ namespace TaskManagement.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Produces("application/json")]
     public class TasksController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -22,7 +23,27 @@ namespace TaskManagement.Api.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Create new task
+        /// </summary>
+        /// <param name="request">Task information</param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>Identifier of newly created taks</returns>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///    PUT /tasks
+        ///    {
+        ///        "title": "Task title",
+        ///        "description": "Task description"
+        ///    }   
+        /// 
+        /// </remarks> 
+        /// <response code="201">Returns newly created task identifier</response>
+        /// <response code="400">If request is invalid</response>
+        /// <response code="500">If something went wrong</response>
         [HttpPut]
+        [ProducesResponseType(typeof(CreateTaskResponse), 201)]
         public async Task<IActionResult> CreateTask([FromBody] CreateTaskRequest request,
             CancellationToken cancellationToken = default)
         {
@@ -40,7 +61,18 @@ namespace TaskManagement.Api.Controllers
             });
         }
 
+        /// <summary>
+        /// Get task by id
+        /// </summary>
+        /// <param name="id">Task identifier</param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns>Task information</returns>
+        /// <response code="200">Returns task information</response>
+        /// <response code="404">If task with given id not found</response>
+        /// <response code="500">If something went wrong</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(GetTaskByIdResponse), 200)]
+        [ProducesResponseType(typeof(ErrorModel), 404)]
         public async Task<IActionResult> GetTaskById(string id, CancellationToken cancellationToken = default)
         {
             GetTaskByIdQuery query = new()
--- TaskManagement.Api/Program.cs
+++ TaskManagement.Api/Program.cs
@@ -1,19 +1,33 @@
+using System;
+using System.IO;
+using System.Reflection;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 using TaskManagement.Domain;
 using TaskManagement.Domain.DbContexts;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Task Management API",
+        Description = "An ASP.NET Core Web API for managing tasks"
+    });
+
+    string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 builder.Services.AddDomain();
 builder.Services.AddDbContext<TaskManagementContext>(
