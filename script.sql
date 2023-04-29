ee487bd430bb1919298d24db252d213c8d3508e2
igorbelovash <belovash2002@gmail.com>
2023-04-29 11:05:17 +0300

    homework

--- script.sql
+++ script.sql
@@ -1,34 +1,42 @@
-INSERT INTO tbl_books(title, author, isbn, year)
-VALUES('The Hunger Games', 'Suzanne Collins', 'Fiction', 2008),
-('Harry Potter and the Order of the Phoenix', 'Joanne Rowling', 'Magic', 2003),
-('The Chronicles of Narnia', 'Clive Lewis', 'Fantasy', 1956),
-('The Picture of Dorian Gray', 'Oscar Wilde', 'Fiction', 1890),
-('Romeo and Juliet', 'William Shakespeare', 'Fiction', 1597),
-('Harry Potter and the Sorcerers Stone', 'Joanne Rowling', 'Magic', 1997),
-('A Game of Thrones', 'George Martin', 'Fantasy', 1996)
+CREATE TABLE IF NOT EXISTS tbl_phone_book (
+	id BIGSERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	phone VARCHAR(15) NOT NULL,
+	email VARCHAR(100)NOT NULL
+)
 
+CREATE TABLE IF NOT EXISTS tbl_school_schedule (
+	id BIGSERIAL PRIMARY KEY,
+	day VARCHAR(50) NOT NULL,
+	class_room VARCHAR(50) NOT NULL,
+	lesson VARCHAR(50) NOT NULL,
+	start_lesson TIME NOT NULL,
+	end_lesson TIME NOT NULL
+)
 
-SELECT isbn, COUNT(*) AS book_count
-FROM tbl_books
-GROUP BY isbn
+CREATE TABLE IF NOT EXISTS tbl_login_history (
+	id BIGSERIAL PRIMARY KEY,
+	user_login VARCHAR(100) NOT NULL,
+	user_email VARCHAR(100) NOT NULL,
+	login_time TIMESTAMP NOT NULL,
+    logout_time TIMESTAMP NOT NULL
+)
 
-SELECT * FROM tbl_books
-WHERE title LIKE '%a%'
+CREATE TABLE IF NOT EXISTS tbl_bank_accounts (
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	birthday DATE NOT NULL CHECK(birthday > '1900-01-01'),
+	phone VARCHAR(15) NOT NULL,
+   	balance MONEY NOT NULL
+)
 
-SELECT * FROM tbl_books
-WHERE author IS NULL
-
-UPDATE tbl_books
-SET year = year + 1
-
-DELETE FROM tbl_books
-WHERE author IS NULL
-
-SELECT COUNT(1)
-FROM tbl_books
-WHERE author LIKE '%o%'
-OR author LIKE '%O%'
-
-SELECT year, COUNT(*) AS book_count
-FROM tbl_books
-GROUP BY year
+CREATE TABLE IF NOT EXISTS tbl_bank_transactions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_number BIGINT NOT NULL,
+    date TIMESTAMP NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    amount MONEY NOT NULL,
+    description VARCHAR(50) NOT NULL
+)
