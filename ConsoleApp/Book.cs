55427e1de140c1d31355ed3d3a93d3c0b63d9fc5
igorbelovash <belovash2002@gmail.com>
2023-08-30 16:25:36 +0300

    get; set;

---
+++ ConsoleApp/Book.cs
@@ -0,0 +1,11 @@
+namespace BookLibrary
+{
+    public class Book
+    {
+        public string BookTitle { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public Author Author { get; set; }
+        
+    }
+}
