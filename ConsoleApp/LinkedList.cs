11f0b186e58730dd2c896419e8a92962e0ed1750
igorbelovash <belovash2002@gmail.com>
2024-01-13 19:00:59 +0200

    generics

--- ConsoleApp/LinkedList.cs
+++ ConsoleApp/LinkedList.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ConsoleApp
 {
     public class LinkedList<T>
@@ -9,30 +11,104 @@ namespace ConsoleApp
         }
 
         private Element _head;
+        public int Length { get; private set; }
 
         public void Add(T item)
         {
-            if (_head == null)
+            Insert(item, Length);
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            Length = 0;
+        }
+
+        public void RemoveAt(int position)
+        {
+            if (position < 0 || position >= Length)
             {
-                _head = new Element
-                {
-                    Value = item
-                };
+                throw new IndexOutOfRangeException($"Index is outside of list");
+            }
+
+            if (position == 0)
+            {
+                _head = _head.Next;
             }
             else
             {
-                Element element = new()
+                Element current = _head;
+                for (int i = 0; i < position - 1; i++)
                 {
-                    Value = item
-                };
-
-                Element last = _head;
-                for (; last.Next != null; last = last.Next)
-                {
+                    current = current.Next;
                 }
 
-                last.Next = element;
+                current.Next = current.Next.Next;
             }
+
+            --Length;
+        }
+
+        public void Insert(T item, int position)
+        {
+            if (position < 0 || position > Length)
+            {
+                throw new IndexOutOfRangeException($"Index is outside of list");
+            }
+
+            Element element = new()
+            {
+                Value = item
+            };
+
+            if (position == 0)
+            {
+                element.Next = _head;
+                _head = element;
+            }
+            else
+            {
+                Element current = _head;
+                for (int i = 0; i < position - 1; i++)
+                {
+                    current = current.Next;
+                }
+
+                element.Next = current.Next;
+                current.Next = element;
+            }
+
+            ++Length;
+        }
+
+        public T[] ToArray()
+        {
+            T[] arr = new T[Length];
+
+            Element current = _head;
+            for (int i = 0; i < Length; i++)
+            {
+                arr[i] = current.Value;
+                current = current.Next;
+            }
+
+            return arr;
+        }
+
+        public void CopyTo(T[] arr)
+        {
+            int length = Length < arr.Length ? Length : arr.Length;
+            Element current = _head;
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", ToArray());
         }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Linq.Expressions;
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
+using System.Xml;
 
 namespace ConsoleApp
 {
@@ -346,49 +347,189 @@ namespace Name
     // }
 }
 
-namespace Generics
-{
-    public class Container<T>
-    {
-        public T Value1 {get; set;}
+// namespace Generics
+// {
+//     public class Container<T>
+//     {
+//         public T Value1 {get; set;}
 
-        public void Add(T item)
-        {
-            Value1 = item;
-        }
-        public void Remove()
-        {
+//         public void Add(T item)
+//         {
+//             Value1 = item;
+//         }
+//         public void Remove()
+//         {
             
-        }
-        public T Get()
-        {
-            return Value1;
-        }
-    }
-}
+//         }
+//         public T Get()
+//         {
+//             return Value1;
+//         }
+//     }
+// }
 
 
-namespace Generics
+// // namespace Generics
+// // {
+// //     public class Pair<T1, T2>
+// //     {
+// //         private T1 _item1;
+// //         private T2 _item2;
+
+// //         public Pair(T1 item1, T2 item2)
+// //         {
+// //             _item1 = item1;
+// //             _item2 = item2;
+// //         }
+// //         public void Swap(Pair<T1, T2> otherPair)
+// //         {
+// //             (this._item1, this._item2, otherPair._item1, otherPair._item2) =
+// //                 (otherPair._item1, otherPair._item2, this._item1, this._item2);
+// //         }
+
+// //         public override string ToString()
+// //         {
+// //             return $"{_item1}, {_item2}";
+// //         }
+// //     }
+
+// //     class Program
+// //     {
+// //         static void Main()
+// //         {
+// //             Pair<int, string> pair1 = new Pair<int, string>(42, "hello");
+// //             Pair<int, string> pair2 = new Pair<int, string>(100, "world");
+
+// //             pair1.Swap(pair2);
+
+// //             Console.WriteLine(pair1.ToString()); // виведе "100, world"
+// //             Console.WriteLine(pair2.ToString()); // виведе "42, hello"
+
+// //             // int i = 0;
+// //             // while (i <= 100) 
+// //             // {
+// //             //     Console.WriteLine(i);
+// //             //     i++;
+// //             // }
+// //         }
+// //     }
+
+// // }
+
+// namespace Generics1
+// {
+//     public class Box<T>
+//     {
+//         private T _content;
+
+//         public Box(T content)
+//         {
+//             _content = content;
+//         }
+
+//         public T GetContent()
+//         {
+//             return _content;
+//         }
+
+//         public void SetContent(T content)
+//         {
+//             _content = content;
+//         }
+
+//         public void DisplayInfo()
+//         {
+//             Console.WriteLine($"Box content: {_content}");
+//         }
+
+//         public override string ToString()
+//         {
+//             return _content.ToString();
+//         }
+//     }
+
+//     class Program
+//     {
+//         static void Main()
+//         {
+//             Box<string> box1 = new("Hello");
+//             Console.WriteLine($"{box1} world!");
+
+//             Box<int> box2 = new(42);
+//             box2.DisplayInfo();
+//         }
+//     }
+// }
+        
+// namespace Name1
+// {
+//     public class Pair<T>
+//     {
+//         public void Swap(ref T int1, ref T int2)
+//         {
+//             (int1, int2) = (int2, int1);
+//         }     
+//     }
+//     class Program
+//     {
+//         static void Main()
+//         {
+//             int i1 = 15;
+//             int i2 = 20;
+//             Pair<int> pair = new();
+//             pair.Swap(ref i1, ref i2);
+//             Console.WriteLine((i1, i2)); 
+//         }
+//     }
+// }
+
+
+namespace Name
 {
-    public class Pair<T1, T2>
+    public class Stack<T>
     {
-        private T1 _item1;
-        private T2 _item2;
-
-        public Pair(T1 item1, T2 item2)
+        private class StackItem
         {
-            _item1 = item1;
-            _item2 = item2;
-        }
-        public void Swap(Pair<T1, T2> otherPair)
-        {
-            (this._item1, this._item2, otherPair._item1, otherPair._item2) =
-                (otherPair._item1, otherPair._item2, this._item1, this._item2);
+            public T Value { get; init; }
+            public StackItem Next { get; init; }
         }
 
-        public override string ToString()
+        private StackItem _stackItem;
+        public int Count { get; private set; }
+
+        public void Push(T value)
         {
-            return $"{_item1}, {_item2}";
+            StackItem stackItem = new()
+            {
+                Value = value,
+                Next = _stackItem
+            };
+            _stackItem = stackItem;
+            ++Count;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            StackItem item = _stackItem;
+            _stackItem = _stackItem.Next;
+            --Count;
+
+            return item.Value;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return _stackItem.Value;
         }
     }
 
@@ -396,14 +537,31 @@ namespace Generics
     {
         static void Main()
         {
-            Pair<int, string> pair1 = new Pair<int, string>(42, "hello");
-            Pair<int, string> pair2 = new Pair<int, string>(100, "world");
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
 
-            pair1.Swap(pair2);
+            Console.WriteLine("Current Stack:");
 
-            Console.WriteLine(pair1.ToString()); // виведе "100, world"
-            Console.WriteLine(pair2.ToString()); // виведе "42, hello"
+            while (intStack.Count > 0)
+            {
+                Console.WriteLine($"Peek: {intStack.Peek()}");
+                Console.WriteLine($"Pop: {intStack.Pop()}");
+            }
+
+            Stack<string> stringStack = new Stack<string>();
+            stringStack.Push("Hello");
+            stringStack.Push("World");
+            stringStack.Push("!");
+
+            Console.WriteLine("\nCurrent Stack:");
+
+            while (stringStack.Count > 0)
+            {
+                Console.WriteLine($"Peek: {stringStack.Peek()}");
+                Console.WriteLine($"Pop: {stringStack.Pop()}");
+            }
         }
     }
-
 }
